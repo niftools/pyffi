594f258a688ea2a55bb2f9efc39fffab339ebbea
Amorilia <amorilia@users.sourceforge.net>
2007-04-26 11:28:02 +0000

    contrib/python: expression parser, initial try

---
+++ niftools/contrib/python/FileFormat/Bases/Expression.py
@@ -0,0 +1,245 @@
+# --------------------------------------------------------------------------
+# FileFormat.Bases.Expression
+# Expression parser.
+# --------------------------------------------------------------------------
+# ***** BEGIN LICENSE BLOCK *****
+#
+# Copyright (c) 2007, NIF File Format Library and Tools.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#
+#    * Neither the name of the NIF File Format Library and Tools
+#      project nor the names of its contributors may be used to endorse
+#      or promote products derived from this software without specific
+#      prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# ***** END LICENCE BLOCK *****
+# --------------------------------------------------------------------------
+
+class Expression(object):
+    """This class represents an expression.
+    
+    >>> class A(object):
+    ...     x = False
+    ...     y = True
+    >>> a = A()
+    >>> e = Expression('x || y', a)
+    >>> int(e)
+    1
+    >>> bool(e)
+    True
+    >>> e = Expression('99 & 15', a)
+    >>> int(e)
+    3
+    >>> e = Expression('(99&15)&&y', a)
+    >>> bool(e)
+    True
+    >>> e = Expression('(99 &15) &&x', a)
+    >>> bool(e)
+    False
+    """
+    operators = [ '==', '!=', '&&', '||', '&', '|' ]
+    def __init__(self, expr_str, data = None):
+        self._data = data
+        left, self._op, right = self._partition(expr_str)
+        self._left = self.parse(left)
+        self._right = self.parse(right)
+
+    def __int__(self):
+        if isinstance(self._left, Expression):
+            left = int(self._left)
+        elif isinstance(self._left, basestring):
+            left = getattr(self._data, self._left)
+        else:
+            assert(isinstance(self._left, int)) # debug
+            left = self._left
+
+        if not self._op:
+            return left
+
+        if isinstance(self._right, Expression):
+            right = int(self._right)
+        elif isinstance(self._right, basestring):
+            right = getattr(self._data, self._right)
+        else:
+            assert(isinstance(self._right, int)) # debug
+            right = self._right
+
+        if self._op == '==':
+            return int(left == right)
+        elif self._op == '!=':
+            return int(left == right)
+        elif self._op == '&&':
+            return int(left and right)
+        elif self._op == '||':
+            return int(left or right)
+        elif self._op == '&':
+            return left & right
+        elif self._op == '|':
+            return left | right
+        else:
+            raise NotImplementedError("expression syntax error: operator '" + op + "' not implemented")
+
+    def __nonzero__(self):
+        return self.__int__()
+
+    @classmethod
+    def parse(cls, expr_str):
+        """Returns an Expression, string, or int, depending on the
+        contents of <expr_str>."""
+        # brackets or operators => expression
+        if ("(" in expr_str) or (")" in expr_str):
+            return Expression(expr_str)
+        for op in cls.operators:
+            if expr_str.find(op) != -1:
+                return Expression(expr_str)
+        # try to convert it to an integer
+        try:
+            return int(expr_str)
+        # failed, so return the string
+        except ValueError:
+            return expr_str
+    
+    @classmethod
+    def _partition(cls, expr_str):
+        """Partitions expr_str. See examples below.
+
+        >>> Expression._partition('abc || efg')
+        ('abc', '||', 'efg')
+        >>> Expression._partition('abc||efg')
+        ('abc', '||', 'efg')
+        >>> Expression._partition('abcdefg')
+        ('abcdefg', '', '')
+        >>> Expression._partition(' abcdefg ')
+        ('abcdefg', '', '')
+        >>> Expression._partition(' (a | b) & c ')
+        ('a | b', '&', 'c')
+        >>> Expression._partition('(a | b)!=(b&c)')
+        ('a | b', '!=', 'b&c')
+        >>> Expression._partition('(a== b) &&(( b!=c)||d )')
+        ('a== b', '&&', '( b!=c)||d')
+        """
+        # check if the left hand side starts with brackets
+        # and if so, find the position of the starting bracket and the ending
+        # bracket
+        left_startpos, left_endpos = cls._scanBrackets(expr_str)
+        if left_startpos >= 0:
+            # yes, it is a bracketted expression
+            # so remove brackets and whitespace,
+            # and let that be the left hand side
+            left_str = expr_str[left_startpos+1:left_endpos].strip()
+            # the next token should be the operator
+            # find the position where the operator should start
+            op_startpos = left_endpos+1
+            while expr_str[op_startpos].isspace():
+                op_startpos += 1
+            # to avoid confusion between && and &, and || and |,
+            # let's first scan for operators of two characters
+            # and then for operators of one character
+            for op_endpos in xrange(op_startpos+1, op_startpos-1, -1):
+                op_str = expr_str[op_startpos:op_endpos+1]
+                if op_str in cls.operators:
+                    break
+            else:
+                raise ValueError("expression syntax error: expected operator at '%s'"%expr_str[op_startpos:])
+        else:
+            # it's not... so we need to scan for the first operator
+            for op_startpos, ch in enumerate(expr_str):
+                if ch.isspace(): continue
+                if ch == '(' or ch == ')':
+                    raise ValueError("expression syntax error: expected operator before '%s'"%expr_str[op_startpos:])
+                # to avoid confusion between && and &, and || and |,
+                # let's first scan for operators of two characters
+                for op_endpos in xrange(op_startpos+1, op_startpos-1, -1):
+                    op_str = expr_str[op_startpos:op_endpos+1]
+                    if op_str in cls.operators:
+                        break
+                else:
+                    continue
+                break
+            else:
+                # no operator found, so we are done
+                left_str = expr_str.strip()
+                op_str = ''
+                right_str = ''
+                return left_str, op_str, right_str
+            # operator found! now get the left hand side
+            left_str = expr_str[:op_startpos].strip()
+
+        # now we have done the left hand side, and the operator
+        # all that is left is to process the right hand side
+        right_startpos, right_endpos = cls._scanBrackets(expr_str, op_endpos+1)
+        if right_startpos >= 0:
+            # yes, it is a bracketted expression
+            # so remove brackets and whitespace,
+            # and let that be the right hand side
+            right_str = expr_str[right_startpos+1:right_endpos].strip()
+            # check for trailing junk
+            if expr_str[right_endpos+1:] and not expr_str[right_endpos+1:].isspace():
+                raise ValueError("expression syntax error: unexpected trailing characters '%s'"%expr_str[right_endpos+1:])
+        else:
+            # no, so just take the whole expression as right hand side
+            right_str = expr_str[op_endpos+1:].strip()
+            # check that it is a valid expression
+            if ("(" in right_str) or (")" in right_str):
+                raise ValueError("expression syntax error: unexpected brackets in '%s'"%right_str)
+        return left_str, op_str, right_str
+
+    @staticmethod
+    def _scanBrackets(expr_str, fromIndex = 0):
+        """Looks for matching brackets.
+
+        >>> Expression._scanBrackets('abcde')
+        (-1, -1)
+        >>> Expression._scanBrackets('()')
+        (0, 1)
+        >>> Expression._scanBrackets('(abc(def))g')
+        (0, 9)
+        >>> s = '  (abc(dd efy 442))xxg'
+        >>> startpos, endpos = Expression._scanBrackets(s)
+        >>> print s[startpos+1:endpos]
+        abc(dd efy 442)
+        """
+        startpos = -1
+        endpos = -1
+        scandepth = 0
+        for scanpos, scanchar in enumerate(expr_str):
+            if scanpos < fromIndex: continue # TODO: optimize this...
+            if scanchar.isspace(): continue
+            elif scanchar == "(":
+                if startpos == -1:
+                    startpos = scanpos
+                scandepth += 1
+            elif scanchar == ")":
+                scandepth -= 1
+                if scandepth == 0:
+                    endpos = scanpos
+                    break
+        else:
+            if startpos != -1 or endpos != -1:
+                raise ValueError("expression syntax error (non-matching brackets?)")
+        return (startpos, endpos)
--- niftools/contrib/python/rundoctest.py
+++ niftools/contrib/python/rundoctest.py
@@ -2,9 +2,10 @@ import unittest
 import doctest
 import NifFormat.NifFormat
 import FileFormat.Bases.Compound
+import FileFormat.Bases.Expression
 
 suite = unittest.TestSuite()
-for mod in [ NifFormat.NifFormat, FileFormat.Bases.Compound ]:
+for mod in [ NifFormat.NifFormat, FileFormat.Bases.Compound, FileFormat.Bases.Expression ]:
     suite.addTest(doctest.DocTestSuite(mod))
 runner = unittest.TextTestRunner(verbosity=10)
 runner.run(suite)
