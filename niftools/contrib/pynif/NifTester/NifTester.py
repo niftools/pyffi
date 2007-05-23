03c51464584a6a10dc8bbcad10ceca30df2e680c
Amorilia <amorilia@users.sourceforge.net>
2007-05-23 16:46:11 +0000

    new NifTester (moved from pynif/test and pynif/runtest.py)

--- niftools/contrib/pynif/NifTester/NifTester.py
+++ niftools/contrib/pynif/NifTester/NifTester.py
@@ -31,10 +31,10 @@ if __name__ == "__main__":
         print """
 Run arbitrary tests on files by scriptable testers.
 ---
-Syntax: runtest.py <tester> [<file> | <dir>]
+Syntax: NifTester.py <tester> [<file> | <dir>]
 ---
 Usage:  Specify a nif file or folder as first argument.
-        runtest will look for a file called "test/hacking/<tester>.py" or
+        NifTester will look for a file called "test/hacking/<tester>.py" or
         "test/validate/<tester>.py" and use it for testing the specified
         file, or all nif files in the specified folder.
         If no file or folder is specified, the current directory is
@@ -55,41 +55,41 @@ Usage:  Specify a nif file or folder as first argument.
         * check if the library can read all files (python version of
           nifskope's xml checker):
 
-            python runtest.py read
+            python NifTester.py read
 
         * same as above, but also find out profile information on reading nif
           files:
 
-            python -m cProfile -s cumulative runtest.py read
+            python -m cProfile -s cumulative NifTester.py read
 
         * find out time spent on a particular test:
 
-            python -m cProfile -s cumulative runtest.py skindatacheck | grep skindatacheck
+            python -m cProfile -s cumulative NifTester.py skindatacheck | grep skindatacheck
 
 """
         sys.exit(1)
 
-    tester_str = sys.argv[1]
+    test_str = sys.argv[1]
     if len(sys.argv) >= 3:
         top = sys.argv[2]
     else:
         top = '.'
 
     try:
-        test = __import__('test.hacking.' + tester_str)
-        tester = getattr(test.hacking, tester_str)
+        testers = __import__('testers.hacking.' + test_str)
+        test = getattr(testers.hacking, test_str)
         onreaderror = pass_exception
     except ImportError:
         try:
-            test = __import__('test.validate.' + tester_str)
-            tester = getattr(test.validate, tester_str)
+            testers = __import__('testers.validate.' + test_str)
+            test = getattr(testers.validate, test_str)
             onreaderror = raise_exception
         except ImportError:
-            print "Tester '%s' was not found!" % tester_str
+            print "Tester '%s' was not found!" % test_str
             sys.exit(1)
 
-    testBlock = getattr(tester, 'testBlock', None)
-    testRoot = getattr(tester, 'testRoot', None)
-    testFile = getattr(tester, 'testFile', None)
+    testBlock = getattr(test, 'testBlock', None)
+    testRoot = getattr(test, 'testRoot', None)
+    testFile = getattr(test, 'testFile', None)
 
     testPath(top, testBlock, testRoot, testFile, onreaderror)
