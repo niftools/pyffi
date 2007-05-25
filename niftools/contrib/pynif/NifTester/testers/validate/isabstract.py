8c2e93463def030d1b4574b54427180a63243224
Amorilia <amorilia@users.sourceforge.net>
2007-05-25 13:35:37 +0000

    added a test for checking correct abstractness

---
+++ niftools/contrib/pynif/NifTester/testers/validate/isabstract.py
@@ -0,0 +1,7 @@
+# checks for abstract blocks
+
+from NifFormat.NifFormat import NifFormat
+
+def testBlock(block, verbose):
+    if block._isAbstract:
+        raise ValueError('block %s marked as abstract in xml but used in nif file'%block.__class__.__name__)
