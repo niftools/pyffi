57132b504615365dcede4f7ba68d47d160f63aff
Amorilia <amorilia@users.sourceforge.net>
2007-05-29 17:42:29 +0000

    PyTriStrip uses NvTriStrip if present

--- niftools/pynif/NifFormat/PyTriStrip/PyTriStrip.py
+++ niftools/pynif/NifFormat/PyTriStrip/PyTriStrip.py
@@ -1,5 +1,10 @@
-from TraversalUtilities import Stripifier
-from Mesh import Mesh
+try:
+    import NvTriStrip
+    USE_NVTRISTRIP = True
+except ImportError:
+    from TraversalUtilities import Stripifier
+    from Mesh import Mesh
+    USE_NVTRISTRIP = False
 
 def triangulate(strips):
     """A generator for iterating over the faces in a set of
@@ -13,15 +18,13 @@ def triangulate(strips):
 
     for strip in strips:
         i = strip.__iter__()
-        j = True
+        j = False
         t1, t2 = i.next(), i.next()
         for k in xrange(2, len(strip)):
-            t0, t1, t2 = t1, t2, i.next()
-            if t0 == t1 or t1 == t2 or t2 == t0:
-                j = not j
-                continue
-            triangles.append([t0, t1, t2] if j else [t0, t2, t1])
             j = not j
+            t0, t1, t2 = t1, t2, i.next()
+            if t0 == t1 or t1 == t2 or t2 == t0: continue
+            triangles.append([t0, t1, t2] if j else [t0, t2, t1])
 
     return triangles
 
@@ -30,37 +33,73 @@ def _generateFacesFromTriangles(triangles):
     while True:
         yield [i.next(), i.next(), i.next()]
 
-def strippify(faces):
-    """Converts faces into a list of strips.
+def _checkStrips(triangles, strips):
+    strips_triangles = triangulate(strips)
+    for t0,t1,t2 in triangles:
+        if t0 == t1 or t1 == t2 or t2 == t0: continue
+        if [t0,t1,t2] not in strips_triangles and [t1,t2,t0] not in strips_triangles and [t2,t0,t1] not in strips_triangles:
+            raise ValueError('triangle %s in triangles but not in strips\ntriangles = %s\nstrips = %s'%([t0,t1,t2],triangles,strips))
+    for t0,t1,t2 in strips_triangles:
+        if t0 == t1 or t1 == t2 or t2 == t0: continue
+        if [t0,t1,t2] not in triangles and [t1,t2,t0] not in triangles and [t2,t0,t1] not in triangles:
+            raise ValueError('triangle %s in strips but not in triangles\ntriangles = %s\nstrips = %s'%([t0,t1,t2],triangles,strips))
 
-    >>> faces = [[1, 5, 2], [5, 2, 6], [5, 9, 6], [9, 6, 10], [9, 13, 10], [13, 10, 14], [0, 4, 1], [4, 1, 5], [4, 8, 5], [8, 5, 9], [8, 12, 9], [12, 9, 13], [2, 6, 3], [6, 3, 7], [6, 10, 7], [10, 7, 11], [10, 14, 11], [14, 11, 15]]
-    >>> strippify(faces)
-    [[1, 0, 1, 4, 5, 8, 9, 12, 13], [2, 1, 2, 5, 6, 9, 10, 13, 14], [3, 2, 3, 6, 7, 10, 11, 14, 15]]
-    >>> strippify([[0,1,4],[1,2,4],[2,3,4],[3,0,4]])
-    [[3, 0, 4, 1, 2], [2, 3, 4]]
-    >>> strippify([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14], [15, 16, 17], [18, 19, 20], [21, 22, 23]])
-    [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14], [15, 16, 17], [18, 19, 20], [21, 22, 23]]
+def strippify(triangles):
+    """Converts triangles into a list of strips.
+
+    >>> NvTriStrip.SetStitchStrips(False)
+    >>> triangles = [[0,1,4],[1,2,4],[2,3,4],[3,0,4]]
+    >>> strips = strippify(triangles)
+    >>> strips
+    [[0, 1, 4, 2, 4, 3, 0]]
+    >>> _checkStrips(triangles, strips)
+    >>> triangles = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14], [15, 16, 17], [18, 19, 20], [21, 22, 23]]
+    >>> strips = strippify(triangles)
+    >>> _checkStrips(triangles, strips)
     >>> strippify([[0, 1, 2], [0, 1, 2]])
-    [[0, 1, 2], [0, 1, 2]]
+    [[0, 1, 2]]
     >>> strippify([[0, 1, 2], [2, 1, 0]])
     [[0, 1, 2, 0]]
-    >>> strippify([[0, 1, 2], [0, 2, 3], [4, 0, 3], [0, 4, 3]])
-    None
+    >>> triangles = [[0, 1, 2], [2, 1, 0], [1, 2, 3]]
+    >>> strips = strippify(triangles)
+    >>> _checkStrips(triangles, strips) #doctest: +ELLIPSIS
+    Traceback (most recent call last):
+       ...
+    ValueError: ...
+    >>> triangles = [[1, 5, 2], [5, 2, 6], [5, 9, 6], [9, 6, 10], [9, 13, 10], [13, 10, 14], [0, 4, 1], [4, 1, 5], [4, 8, 5], [8, 5, 9], [8, 12, 9], [12, 9, 13], [2, 6, 3], [6, 3, 7], [6, 10, 7], [10, 7, 11], [10, 14, 11], [14, 11, 15]]
+    >>> strips = strippify(triangles)
+    >>> _checkStrips(triangles, strips) #doctest: +ELLIPSIS
+    Traceback (most recent call last):
+       ...
+    ValueError: ...
     """
 
-    # build a mesh from triangles
-    mesh = Mesh()
-    for face in faces:
-        mesh.AddFace(*face)
+    if USE_NVTRISTRIP:
+        lst = []
+        for face in triangles:
+            lst.extend(face)
+        pgroups = NvTriStrip.GenerateStrips(lst, validateEnabled = True)
+        strips = []
+        for ptype, indices in pgroups:
+            if ptype == NvTriStrip.PT_STRIP:
+                strips.append(indices)
+            else:
+                raise RuntimeError("unexpected primitive group type %i"%ptype)
+        return strips
+    else:
+        # build a mesh from triangles
+        mesh = Mesh()
+        for face in triangles:
+            mesh.AddFace(*face)
 
-    # calculate the strip
-    stripifier = Stripifier()
-    stripifier.Selector.MinStripLength = 0
-    stripifier(mesh)
+        # calculate the strip
+        stripifier = Stripifier()
+        stripifier.Selector.MinStripLength = 0
+        stripifier(mesh)
 
-    # add the triangles to it
-    i = stripifier.TriangleList.__iter__()
-    return [face for face in _generateFacesFromTriangles(stripifier.TriangleList)] + stripifier.TriangleStrips
+        # add the triangles to it
+        i = stripifier.TriangleList.__iter__()
+        return [face for face in _generateFacesFromTriangles(stripifier.TriangleList)] + stripifier.TriangleStrips
 
 if __name__=='__main__':
     import doctest
