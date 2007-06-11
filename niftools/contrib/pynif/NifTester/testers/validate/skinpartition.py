840a679f30a75d4bad01da6ef2edf12913a0e6f1
Amorilia <amorilia@users.sourceforge.net>
2007-06-11 16:52:10 +0000

    NifTester: skin partitioning test

---
+++ niftools/contrib/pynif/NifTester/testers/validate/skinpartition.py
@@ -0,0 +1,22 @@
+# check skin partition and skin partition calculation
+
+from NifFormat.NifFormat import NifFormat
+
+import struct
+
+def testBlock(block, verbose):
+    # does it apply on this block?
+    if not isinstance(block, NifFormat.NiTriBasedGeom): return
+    # does this block have a skin?
+    if not block.skinInstance: return
+
+    print "found skin in block '%s'"%block.name
+    block._validateSkin()
+    skininst = block.skinInstance
+    skinpart = skininst.skinPartition
+
+    # TODO copy the skin partition data
+
+    print 'lost weight from partitioning', block.updateSkinPartition(maxbonesperpartition = 4, maxbonespervertex = 4)
+
+    # TODO check the skin partition data
