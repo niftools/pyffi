3fd3d2e3d95af2665c29a492c0405246d15e90a3
Amorilia <amorilia@users.sourceforge.net>
2007-06-27 16:10:29 +0000

    removed isabstract test (the isabstract tag is ignored by PyFFI)

--- niftools/contrib/pynif/NifTester/testers/validate/isabstract.py
+++
@@ -1,7 +0,0 @@
-# checks for abstract blocks
-
-from PyFFI.NIF import NifFormat
-
-def testBlock(block, verbose):
-    if block._isAbstract:
-        raise ValueError('block %s marked as abstract in xml but used in nif file'%block.__class__.__name__)
