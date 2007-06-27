f6679e41dba68f0271fe5b40e143f63fd206bfe5
Amorilia <amorilia@users.sourceforge.net>
2007-06-27 20:56:43 +0000

    forgot to add file with last commit: here's the tester

---
+++ niftools/contrib/pynif/NifTester/testers/validate/skincenterradius.py
@@ -0,0 +1,27 @@
+# run the stripifier on all triangles from nif files
+# also useful for profiling
+
+from PyFFI.NIF import NifFormat
+
+def testBlock(block, verbose):
+    if not isinstance(block, NifFormat.NiTriBasedGeom): return
+    if not block.isSkin(): return
+
+    print "getting skindata block bounding sphere"
+    center = []
+    radius = []
+    for skindatablock in block.skinInstance.data.boneList:
+        center.append(skindatablock.boundingSphereOffset)
+        radius.append(skindatablock.boundingSphereRadius)
+
+    print "recalculating bounding sphere"
+    block.updateSkinCenterRadius()
+
+    print "comparing old bounding sphere with new bounding sphere"
+    for i, skindatablock in enumerate(block.skinInstance.data.boneList):
+        if center[i] != skindatablock.boundingSphereOffset:
+            raise ValueError("center does not match; original %s, calculated %s"%(center[i], skindatablock.boundingSphereOffset))
+        if abs(radius[i] - skindatablock.boundingSphereRadius) > NifFormat._EPSILON:
+            raise ValueError("radius does not match; original %s, calculated %s"%(radius[i], skindatablock.boundingSphereRadius))
+    print "perfect match!"
+
