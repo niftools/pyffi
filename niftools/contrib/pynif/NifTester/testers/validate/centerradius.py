d5a72f0aaeb2250c65ea1925741a248dfe76a9fd
Amorilia <amorilia@users.sourceforge.net>
2007-06-28 17:38:14 +0000

    NifTester: updated centerradius test; for now it just prints error message without raising exception if mismatch is found

--- niftools/contrib/pynif/NifTester/testers/validate/centerradius.py
+++ niftools/contrib/pynif/NifTester/testers/validate/centerradius.py
@@ -1,22 +1,29 @@
-# run the stripifier on all triangles from nif files
-# also useful for profiling
+# check center and radius in nif files
+
+# tentative results
+# -----------------
+# oblivion: ok
+# civ4: mostly ok (with very few exceptions: effects/magpie/flock.nif, units/!errorunit/bear.nif, maybe some more)
+# daoc: ok
+# morrowind: usually ok (quite some exceptions here)
+# zoo tycoon 2: mostly ok (except *_Adult_*.nif files)
 
 from PyFFI.NIF import NifFormat
 
 def testBlock(block, verbose):
     if not isinstance(block, NifFormat.NiGeometry): return
 
-    print "found geometry", block.name
+    print "checking", block.name
     geomdata = block.data
 
-    print "getting bounding sphere"
+    #print "getting bounding sphere"
     center = NifFormat.Vector3()
     center.x = geomdata.center.x
     center.y = geomdata.center.y
     center.z = geomdata.center.z
     radius = geomdata.radius
 
-    print "checking all vertices are inside"
+    #print "checking all vertices are inside"
     maxr = 0.0
     maxv = None
     for v in geomdata.vertices:
@@ -26,21 +33,19 @@ def testBlock(block, verbose):
             maxv = v
     maxr = maxr**0.5
 
-    if maxr > radius + NifFormat._EPSILON:
-       #raise ValueError("not all vertices inside bounding sphere (vertex %s, error %s)"%(v, abs(maxr-radius)))
-       print "!!! not all vertices inside bounding sphere (vertex %s, error %s)"%(v, abs(maxr-radius))
+    if maxr > 1.01*radius + NifFormat._EPSILON:
+       raise ValueError("not all vertices inside bounding sphere (vertex %s, error %s)"%(v, abs(maxr-radius)))
+       #print "!!! not all vertices inside bounding sphere (vertex %s, error %s)"%(v, abs(maxr-radius))
 
-    return
-
-    # following part seems to fail on quite some nifs
-
-    print "recalculating bounding sphere"
+    #print "recalculating bounding sphere"
     geomdata.updateCenterRadius()
 
-    print "comparing old bounding sphere with new bounding sphere"
+    #print "comparing old bounding sphere with new bounding sphere"
     if center != geomdata.center:
-       raise ValueError("center does not match; original %s, calculated %s"%(center, geomdata.center))
+       #raise ValueError("center does not match; original %s, calculated %s"%(center, geomdata.center))
+       print("center does not match; original %s, calculated %s"%(center, geomdata.center))
     if abs(radius - geomdata.radius) > NifFormat._EPSILON:
-       raise ValueError("radius does not match; original %s, calculated %s"%(radius, geomdata.radius))
-    print "perfect match!"
+       #raise ValueError("radius does not match; original %s, calculated %s"%(radius, geomdata.radius))
+       print("radius does not match; original %s, calculated %s"%(radius, geomdata.radius))
+    #print "perfect match!"
 
