482e38b835da5e778538719482f62099533e98ad
Amorilia <amorilia@users.sourceforge.net>
2007-07-29 10:21:54 +0000

    updated setup.py; detect python version; classifiers

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
+"""A Python library for reading, writing, and processing binary files.
+
+PyFFI reads a file format description (XML) and creates customizable
+Python classes to read & write such files (e.g. Netimmerse File Format
+(NIF), Crytek files (CGF)). Includes tools for files used by 3D games
+(e.g. stripifier, tangent space calculation). Only the NIF format is
+supported in this release."""
+
+classifiers = """\
+Development Status :: 3 - Alpha
+License :: OSI-Approved Open Source :: BSD License
+Intended Audience :: by End-User Class :: Developers
+Intended Audience :: by End-User Class :: End Users/Desktop
+Topic :: Formats and Protocols :: Data Formats
+Programming Language :: Python
+Operating System :: OS Independent
+"""
+
+doclines = __doc__.split("\n")
+
 from distutils.core import setup
+import sys
+
+if sys.version_info < (2, 5):
+    raise RuntimeError("PyFFI requires Python 2.5 or higher.")
 
 setup(
     name = "PyFFI",
@@ -7,9 +31,11 @@ setup(
     package_data = { '' : [ '*.xml' ] }, # include xml files
     author = "Python File Format Interface",
     author_email = "amorilia@gamebox.net",
-    description = "A Python library for reading, writing, and processing binary files.",
-    long_description = "PyFFI reads a file format description (XML) and creates customizable Python classes to read & write such files (e.g. Netimmerse File Format (NIF), Crytek files (CGF)). Includes tools for files used by 3D games (e.g. stripifier, tangent space calculation). Only the NIF format is supported in this release.",
     license = "BSD",
-    keywords = "fileformat nif cgf binary interface stripify",
+    keywords = "fileformat nif cgf binary interface stripify", 
+    platforms = ["any"],
+    description = doclines[0],
+    classifiers = filter(None, classifiers.split("\n")),
+    long_description = "\n".join(doclines[2:]),
     url = "http://pyffi.sourceforge.net/"
 )
