6f16e72ff3478367f574278a06ebec3dd7cb8127
Amorilia <amorilia@users.sourceforge.net>
2007-08-09 20:57:22 +0000

    added documentation

--- PyFFI/__init__.py
+++ PyFFI/__init__.py
@@ -1,7 +1,107 @@
 """
-PyFFI provides a metaclass for parsing a file format description in XML format.
-The actual implementation of the parser is delegated to PyFFI.XmlHandler.
-The most common basic types are implemented in PyFFI.Common.
+Essentially, PyFFI provides a metaclass for parsing a file format description in XML format.
+
+Using PyFFI
+===========
+
+If you simply wish to use PyFFI with a format that's already implemented,
+refer to the documentation of that library. For the nif format, refer to
+L{PyFFI.NIF}, and for the cgf format, refer to L{PyFFI.CGF}.
+
+Supporting New File Formats
+===========================
+
+This section tries to explain how you can implement your own format in PyFFI.
+
+Getting Started
+---------------
+
+Suppose you have a simple file format, which consists of an integer, followed
+by a list of integers as many as described by the first integer. We start
+by creating an XML file, call it simple.xml, which describes this format
+in a way that PyFFI can understand::
+
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE fileformat>
+    <fileformat version="1.0">
+        <basic name="int">A signed 32-bit integer.</basic>
+        <struct name="Example">
+            <add name="Num Integers" type="int">Number of integers that follow.</add>
+            <add name="Integers" type="int" arr1="Num Integers">A list of integers.</add>
+        </struct>
+    </fileformat>
+
+What PyFFI does is convert this simple XML description into Python classes
+which automatically can read and write the structure you've just described.
+Say this is the contents of simple.py::
+
+    from PyFFI import MetaXmlFileFormat
+    from PyFFI import Common
+    class SimpleFormat:
+        __metaclass__ = MetaXmlFileFormat
+        xmlFileName = 'simple.xml'
+        xmlFilePath = [ os.path.dirname(__file__) ]
+        clsFilePath = os.path.dirname(__file__)
+
+        int = Common.Int
+
+What happens in this piece of code?
+
+  - The metaclass assignment triggers the transformation of xml into Python
+    classes; how these classes can be used will be explained further.
+  - The xmlFileName class attribute provides the name of the xml file that
+    describes the structures we wish to generate. The xmlFilePath attribute
+    gives a list of locations of where to look for this file; in our case we have
+    simply chosen to put 'simple.xml' in the same directory as 'simple.py'.
+  - The clsFilePath attribute tells PyFFI where to look for class customizers.
+    For instance, we could have another file called Example.py::
+
+        def addInteger(self, x):
+            self.numIntegers += 1
+            self.integers.updateSize()
+            self.integers[self.numIntegers-1] = x
+
+    which would provide the class SimpleFormat.Example with a function addInteger.
+  - Finally, the PyFFI.Common module implements the most common basic types, such 
+    as integers, characters, and floats. In the above example we have taken
+    advantage of Common.Int, which defines a signed 32-bit integer, exactly the
+    type we need.
+
+Reading and Writing Files
+-------------------------
+
+To read and print the contents of a file of the format described by
+simple.xml::
+
+    from simple import SimpleFormat
+    x = SimpleFormat.Simple()
+    f = open('somefile.simple', 'rb')
+    x.read(f = f)
+    f.close()
+    print x
+
+Or, to create a new file in this format::
+
+    from simple import SimpleFormat
+    x = SimpleFormat.Simple()
+    x.numIntegers = 5
+    x.integers.updateSize()
+    x.integers[0] = 3
+    x.integers[1] = 1
+    x.integers[2] = 4
+    x.integers[3] = 1
+    x.integers[4] = 5
+    f = open('pi.simple', 'wb')
+    x.write(f = f)
+    f.close()
+
+Further References
+------------------
+
+With the above simple example in mind, you may wish to browse through the
+source code of
+L{PyFFI.CGF} or L{PyFFI.NIF} to see how PyFFI works for more complex file
+formats.
 """
 
 # ***** BEGIN LICENSE BLOCK *****
@@ -59,6 +159,8 @@ class MetaXmlFileFormat(type):
     subclasses corresponding to different (bit)struct types,
     enum types, basic types, and aliases. See NifFormat.py for an example
     of how to use MetaXmlFileFormat.
+
+    The actual implementation of the parser is delegated to PyFFI.XmlHandler.
     """
     
     def __init__(cls, name, bases, dct):
