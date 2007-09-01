b2667c7408b35e102059d04a91384543de5c0b28
Amorilia <amorilia@users.sourceforge.net>
2007-09-01 12:06:54 +0000

    NifTester: mopp checks for getTree and setTree functions

--- niftools/contrib/pynif/NifTester/testers/validate/mopp.py
+++ niftools/contrib/pynif/NifTester/testers/validate/mopp.py
@@ -2,7 +2,12 @@ from PyFFI.NIF import NifFormat
 
 def testBlock(block, verbose):
     if not isinstance(block, NifFormat.bhkMoppBvTreeShape): return
+    print "found a mopp"
+    block.printTree()
 
+    tree = block.getTree()
+
+    mopp = [b for b in block.moppData]
     o = NifFormat.Vector3()
     o.x = block.origin.x
     o.y = block.origin.y
@@ -16,3 +21,16 @@ def testBlock(block, verbose):
 
     if block.origin != o: raise ValueError("origin not correctly recalculated")
     if abs(block.scale - scale) > 0.1: raise ValueError("scale not correctly recalculated")
+
+    print "resetting tree"
+    block.setTree(tree)
+
+    if len(mopp) != block.moppDataSize:
+        block.printTree()
+        raise ValueError("mopp set failed; tree was modified")
+
+    for b1, b2 in zip(mopp, block.moppData):
+        if b1 != b2:
+            block.printTree()
+            raise ValueError("mopp set failed; tree was modified")
+
