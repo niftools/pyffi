aa7ef0d5ddd0432be72f33885c1204dccd8bf6fd
Amorilia <amorilia@users.sourceforge.net>
2007-09-01 18:58:54 +0000

    NifTester: updated mopp tester with new functions in PyFFI

--- niftools/contrib/pynif/NifTester/testers/validate/mopp.py
+++ niftools/contrib/pynif/NifTester/testers/validate/mopp.py
@@ -3,9 +3,6 @@ from PyFFI.NIF import NifFormat
 def testBlock(block, verbose):
     if not isinstance(block, NifFormat.bhkMoppBvTreeShape): return
     print "found a mopp"
-    block.printTree()
-
-    tree = block.getTree()
 
     mopp = [b for b in block.moppData]
     o = NifFormat.Vector3()
@@ -22,15 +19,10 @@ def testBlock(block, verbose):
     if block.origin != o: raise ValueError("origin not correctly recalculated")
     if abs(block.scale - scale) > 0.1: raise ValueError("scale not correctly recalculated")
 
-    print "resetting tree"
-    block.setTree(tree)
+    print "parsing mopp"
+    # n = number of bytes, tris = triangle indices
+    n, tris = block.parseTree(verbose = True)
 
-    if len(mopp) != block.moppDataSize:
-        block.printTree()
-        raise ValueError("mopp set failed; tree was modified")
-
-    for b1, b2 in zip(mopp, block.moppData):
-        if b1 != b2:
-            block.printTree()
-            raise ValueError("mopp set failed; tree was modified")
+    if n != block.moppDataSize:
+        raise ValueError("mopp parsing failed; not all bytes processed")
 
