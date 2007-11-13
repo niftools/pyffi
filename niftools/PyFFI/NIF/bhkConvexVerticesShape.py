11e59bd40eb0a9a4d69652fa1068410d0b1d693c
Amorilia <amorilia@users.sourceforge.net>
2007-11-13 15:41:49 +0000

    pyffi nif: find center of gravity and area of convex vertices shape via quickhull algorithm (wip)

--- niftools/PyFFI/NIF/bhkConvexVerticesShape.py
+++ niftools/PyFFI/NIF/bhkConvexVerticesShape.py
@@ -1,7 +1,5 @@
-# --------------------------------------------------------------------------
-# NifFormat.bhkConvexVerticesShape
-# Custom functions for bhkConvexVerticesShape.
-# --------------------------------------------------------------------------
+"""Custom functions for bhkConvexVerticesShape."""
+
 # ***** BEGIN LICENSE BLOCK *****
 #
 # Copyright (c) 2007, NIF File Format Library and Tools.
@@ -38,7 +36,9 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # ***** END LICENCE BLOCK *****
-# --------------------------------------------------------------------------
+
+from itertools import izip
+argmax = lambda array: max(izip(array, xrange(len(array))))[1] # used in qhull
 
 def applyScale(self, scale):
     """Apply scale factor on data."""
@@ -50,3 +50,58 @@ def applyScale(self, scale):
     for n in self.normals:
         n.w *= scale
 
+def getCenterArea(self):
+    """Calculate center of gravity and area."""
+
+    def qhull(vertices, normal):
+        """Simple implementation of the quickhull algorithm in 3 dimensions for
+        a set of coplanar points. All vertices must satisfy vert * normal = 0.
+        Returns a fan of vertices that makes up the surface."""
+        # adapted from
+        # http://en.literateprograms.org/Quickhull_(Python,_arrays)
+        def dome(verts, normal, base):
+            a, b = base
+            dists = [ normal.crossproduct(b-a) * vert for vert in verts ]
+            outer = [ vert for vert, dist in izip(verts, dists) if dist > 0 ]
+ 
+            if len(outer):
+                pivot = sample[argmax(dists)]
+                return dome(outer, [a, pivot]) \
+                       + dome(outer, [pivot, b])[1:]
+            else:
+                return [base]
+  
+        if len(vertices) > 2:
+            a, b = vertices[:2]
+            return dome(vertices, [a, b]) + dome(vertices, [b, a])[1:]
+        else:
+            return vertices
+
+    centerarea = []
+    # iterate over all half spaces
+    for norm in self.normals:
+        # find vertices that lie on this plane
+        normverts = []
+        for vert in self.vertices:
+            if abs(norm.x * vert.x + norm.y * vert.y + norm.z * vert.z
+                   + norm.w) < 0.001:
+                normverts.append(vert)
+        assert(len(normverts) >= 3) # debug
+        
+        # find center and area of each triangle that makes up this set of
+        # coplanar vertices; note that this set is already convex, the
+        # qhull algorithm is simply used to sort the vertices into a fan
+        fan = qhull(normverts)
+        for i in xrange(2, len(fan)):
+            centerarea.append(
+                ( (fan[0] + fan[i-1] + fan[i]) / 3,
+                  (fan[i-1] - fan[0]).crossproduct(fan[i] - fan[0]).norm() / 2 ))
+    # now return the average center and total area
+    totalarea = sum(area for center, area in centerarea)
+    return ( [ sum(area * center.x for center, area in centerarea)
+               / totalarea,
+               sum(area * center.y for center, area in centerarea)
+               / totalarea,
+               sum(area * center.z for center, area in centerarea)
+               / totalarea ],
+             totalarea )
