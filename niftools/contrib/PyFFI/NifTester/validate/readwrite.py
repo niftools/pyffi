8bd3816c17bdddc18a04630e506d87b8d2d3215d
Amorilia <amorilia@users.sourceforge.net>
2007-12-16 10:12:22 +0000

    niftester: readwrite file size check bug fix

--- niftools/contrib/PyFFI/NifTester/validate/readwrite.py
+++ niftools/contrib/PyFFI/NifTester/validate/readwrite.py
@@ -9,12 +9,14 @@ def testFile(stream, version = None, user_version = None, roots = None, **args):
         NifFormat.write(
             f_tmp,
             version = version, user_version = user_version, roots = roots)
-        stream.seek(2,0)
-        f_tmp.seek(2,0)
         # comparing the files will usually be different because blocks may
         # have been written back in a different order, so cheaply just compare
         # file sizes
+        stream.seek(0,2)
+        f_tmp.seek(0,2)
         if stream.tell() != f_tmp.tell():
+            print "original size: %i" % stream.tell()
+            print "written size:  %i" % f_tmp.tell()
             raise StandardError('write check failed: file sizes differ')
     finally:
         f_tmp.close()
