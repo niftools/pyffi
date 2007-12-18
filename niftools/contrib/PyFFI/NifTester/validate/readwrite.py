7c08457062b2a350c5d13bdc74ed48d837903ee2
Amorilia <amorilia@users.sourceforge.net>
2007-12-18 09:22:24 +0000

    improved nif readwrite test for oblivion

--- niftools/contrib/PyFFI/NifTester/validate/readwrite.py
+++ niftools/contrib/PyFFI/NifTester/validate/readwrite.py
@@ -12,11 +12,27 @@ def testFile(stream, version = None, user_version = None, roots = None, **args):
         # comparing the files will usually be different because blocks may
         # have been written back in a different order, so cheaply just compare
         # file sizes
+
+        # header size can be different due to export info
+        # so first read original header
+        stream.seek(0)
+        orig_hdr = NifFormat.Header()
+        orig_hdr.read(stream, version = version, user_version = user_version)
+        orig_hdr_size = orig_hdr.getSize(version = version, user_version = user_version)
+        f_tmp.seek(0)
+        tmp_hdr = NifFormat.Header()
+        tmp_hdr.read(f_tmp, version = version, user_version = user_version)
+        tmp_hdr_size = tmp_hdr.getSize(version = version, user_version = user_version)
+
         stream.seek(0,2)
         f_tmp.seek(0,2)
-        if stream.tell() != f_tmp.tell():
-            print "original size: %i" % stream.tell()
-            print "written size:  %i" % f_tmp.tell()
+        if stream.tell() - orig_hdr_size != f_tmp.tell() - tmp_hdr_size:
+            print "original size (excluding header): %i" % stream.tell() - orig_hdr_size
+            print "written size (excluding header):  %i" % f_tmp.tell() - tmp_hdr_size
+            f_tmp.seek(0)
+            f_debug = open("debug.nif", "wb")
+            f_debug.write(f_tmp.read(-1))
+            f_debug.close()
             raise StandardError('write check failed: file sizes differ')
     finally:
         f_tmp.close()
