9ea32449a454873e764fc131caee9f437f42a5b7
Amorilia <amorilia@users.sourceforge.net>
2007-12-23 20:49:45 +0000

    updated inertia niftoaster test with new InertiaMatrix interface

--- niftools/contrib/PyFFI/NifTester/validate/bhkbodycenter.py
+++ niftools/contrib/PyFFI/NifTester/validate/bhkbodycenter.py
@@ -8,22 +8,22 @@ def testBlock(block, **args):
     print "getting rigid body mass, center, and inertia"
     mass = block.mass
     center = block.center.getCopy()
-    inertia = [ x for x in block.inertia ]
+    inertia = block.inertia.getCopy()
 
     print "recalculating..."
     block.updateMassCenterInertia(mass = block.mass)
 
-    print "checking mass..."
-    if mass != block.mass:
-        #raise ValueError("center does not match; original %s, calculated %s"%(center, block.center))
-        print("warning: mass does not match; original %s, calculated %s"%(mass, block.mass))
-        # adapt calculated inertia matrix with observed mass
-        if mass > 0.001:
-            correction = mass / block.mass
-            for i in xrange(12):
-                block.inertia[i] *= correction
-    else:
-        print "perfect match!"
+    #print "checking mass..."
+    #if mass != block.mass:
+    #    #raise ValueError("center does not match; original %s, calculated %s"%(center, block.center))
+    #    print("warning: mass does not match; original %s, calculated %s"%(mass, block.mass))
+    #    # adapt calculated inertia matrix with observed mass
+    #    if mass > 0.001:
+    #        correction = mass / block.mass
+    #        for i in xrange(12):
+    #            block.inertia[i] *= correction
+    #else:
+    #    print "perfect match!"
 
     print "checking center..."
     if center != block.center:
@@ -34,9 +34,9 @@ def testBlock(block, **args):
 
     print "checking inertia..."
 
-    scale = max(abs(x) for x in inertia + [ y for y in block.inertia ])
-    if max(abs(x - y) for x, y in zip(inertia, block.inertia)) > 0.1 * scale:
+    scale = max(max(abs(x) for x in row) for row in inertia.asList() + block.inertia.asList())
+    if max(max(abs(x - y) for x, y in zip(row1, row2)) for row1, row2 in zip(inertia.asList(), block.inertia.asList())) > 0.1 * scale:
         #raise ValueError("center does not match; original %s, calculated %s"%(center, block.center))
-        print("warning: inertia does not match:\n\noriginal\n%s\n\ncalculated\n%s\n"%(inertia, [x for x in block.inertia]))
+        print("warning: inertia does not match:\n\noriginal\n%s\n\ncalculated\n%s\n"%(inertia, block.inertia))
     else:
         print "perfect match!"
