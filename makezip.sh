1dbd8b9da37e837037907a0f4abb08eeb79d2b3b
Amorilia <amorilia@users.sourceforge.net>
2008-01-10 11:57:10 +0000

    added postinstall script arg to installer build

--- makezip.sh
+++ makezip.sh
@@ -3,5 +3,5 @@
 rm MANIFEST
 python setup.py sdist --format=zip
 python setup.py sdist --format=bztar
-python setup.py bdist_wininst
+python setup.py bdist_wininst --install-script pyffipostinstallation.py
 
