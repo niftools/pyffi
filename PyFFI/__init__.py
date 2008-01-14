9b8dd3866742b29f9461e6e48a74696ee6af4ddb
Amorilia <amorilia@users.sourceforge.net>
2008-01-14 11:26:58 +0000

    changelog update + 2008 license

--- PyFFI/__init__.py
+++ PyFFI/__init__.py
@@ -114,7 +114,7 @@ formats.
 
 # ***** BEGIN LICENSE BLOCK *****
 #
-# Copyright (c) 2007, Python File Format Interface
+# Copyright (c) 2007-2008, Python File Format Interface
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
