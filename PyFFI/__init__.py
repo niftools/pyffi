cb6f3918c8921a18a8a886fac74c5529074772c7
Amorilia <amorilia@users.sourceforge.net>
2008-01-25 22:22:33 +0000

    new variable: __hexversion__

--- PyFFI/__init__.py
+++ PyFFI/__init__.py
@@ -152,6 +152,8 @@ formats.
 __all__ = [ 'XmlHandler', 'Utils', 'Common', 'Bases' ]
 
 __version__ = '0.8.0'
+__hexversion__ = eval('0x%02X%02X%02X'
+                      % tuple(int(x) for x in __version__.split('.')))
 
 from XmlHandler import XmlSaxHandler
 
