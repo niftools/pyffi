a421fafb59ea7ffc4483424e62c1825585e69735
Amorilia <amorilia@users.sourceforge.net>
2008-02-17 21:11:19 +0000

    pyffi version bump + fixed blender cgf export to make use of new bitstructs

--- PyFFI/__init__.py
+++ PyFFI/__init__.py
@@ -152,7 +152,7 @@ formats.
 
 __all__ = [ 'XmlHandler', 'Utils', 'Common', 'Bases' ]
 
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 __hexversion__ = eval('0x%02X%02X%02X'
                       % tuple(int(x) for x in __version__.split('.')))
 
