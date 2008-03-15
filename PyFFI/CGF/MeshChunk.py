27d4dc24d6ae93de6b20082fc155665c64dbe12f
Amorilia <amorilia@users.sourceforge.net>
2008-03-15 17:59:00 +0000

    MeshChunk setGeometry: fixed some bugs and added doctest

--- PyFFI/CGF/MeshChunk.py
+++ PyFFI/CGF/MeshChunk.py
@@ -39,6 +39,7 @@
 
 import itertools
 from itertools import izip
+from PyFFI.Utils import MathUtils
 
 def applyScale(self, scale):
     """Apply scale factor on data."""
@@ -162,6 +163,185 @@ def setGeometry(self,
     @param matlist: A list of material indices.
     @param uvslist: A list of lists of uvs (one list per material).
     @param colorslist: A list of lists of colors (one list per material).
+
+    >>> from PyFFI.CGF import CgfFormat
+    >>> chunk = CgfFormat.MeshChunk()
+    >>> vertices1 = [(0,0,0),(0,1,0),(1,0,0),(1,1,0)]
+    >>> vertices2 = [(0,0,1),(0,1,1),(1,0,1),(1,1,1)]
+    >>> normals1 = [(0,0,-1),(0,0,-1),(0,0,-1),(0,0,-1)]
+    >>> normals2 = [(0,0,1),(0,0,1),(0,0,1),(0,0,1)]
+    >>> triangles1 = [(0,1,2),(2,1,3)]
+    >>> triangles2 = [(0,1,2),(2,1,3)]
+    >>> uvs1 = [(0,0),(0,1),(1,0),(1,1)]
+    >>> uvs2 = [(0,0),(0,1),(1,0),(1,1)]
+    >>> chunk.setGeometry(verticeslist = [vertices1, vertices2],
+    ...                   normalslist = [normals1, normals2],
+    ...                   triangleslist = [triangles1, triangles2],
+    ...                   matlist = [2,5])
+    >>> print chunk # doctest: +ELLIPSIS
+    <class 'PyFFI.XmlHandler.MeshChunk'> instance at ...
+    * hasVertexWeights : False
+    * hasVertexColors : False
+    * inWorldSpace : False
+    * reserved1 : 0
+    * reserved2 : 0
+    * flags1 : 0
+    * flags2 : 0
+    * numVertices : 8
+    * numIndices : 12
+    * numUvs : 0
+    * numFaces : 4
+    * material : None
+    * numMeshSubsets : 2
+    * meshSubsets : <class 'PyFFI.XmlHandler.MeshSubsetsChunk'> instance at ...
+    * vertAnim : None
+    * vertices :
+        <class 'PyFFI.Bases.Array.Array'> instance at ...
+        0: <class 'PyFFI.XmlHandler.Vertex'> instance at ...
+        * p : [  0.000  0.000  0.000 ]
+        * n : [  0.000  0.000 -1.000 ]
+        1: <class 'PyFFI.XmlHandler.Vertex'> instance at ...
+        * p : [  0.000  1.000  0.000 ]
+        * n : [  0.000  0.000 -1.000 ]
+        2: <class 'PyFFI.XmlHandler.Vertex'> instance at ...
+        * p : [  1.000  0.000  0.000 ]
+        * n : [  0.000  0.000 -1.000 ]
+        3: <class 'PyFFI.XmlHandler.Vertex'> instance at ...
+        * p : [  1.000  1.000  0.000 ]
+        * n : [  0.000  0.000 -1.000 ]
+        4: <class 'PyFFI.XmlHandler.Vertex'> instance at ...
+        * p : [  0.000  0.000  1.000 ]
+        * n : [  0.000  0.000  1.000 ]
+        5: <class 'PyFFI.XmlHandler.Vertex'> instance at ...
+        * p : [  0.000  1.000  1.000 ]
+        * n : [  0.000  0.000  1.000 ]
+        6: <class 'PyFFI.XmlHandler.Vertex'> instance at ...
+        * p : [  1.000  0.000  1.000 ]
+        * n : [  0.000  0.000  1.000 ]
+        7: <class 'PyFFI.XmlHandler.Vertex'> instance at ...
+        * p : [  1.000  1.000  1.000 ]
+        * n : [  0.000  0.000  1.000 ]
+    * faces :
+        <class 'PyFFI.Bases.Array.Array'> instance at ...
+        0: <class 'PyFFI.XmlHandler.Face'> instance at ...
+        * v0 : 0
+        * v1 : 1
+        * v2 : 2
+        * material : 2
+        * smGroup : 1
+        1: <class 'PyFFI.XmlHandler.Face'> instance at ...
+        * v0 : 2
+        * v1 : 1
+        * v2 : 3
+        * material : 2
+        * smGroup : 1
+        2: <class 'PyFFI.XmlHandler.Face'> instance at ...
+        * v0 : 4
+        * v1 : 5
+        * v2 : 6
+        * material : 5
+        * smGroup : 1
+        3: <class 'PyFFI.XmlHandler.Face'> instance at ...
+        * v0 : 6
+        * v1 : 5
+        * v2 : 7
+        * material : 5
+        * smGroup : 1
+    * uvs : <class 'PyFFI.Bases.Array.Array'> instance at ...
+    * verticesData : <class 'PyFFI.XmlHandler.DataStreamChunk'> instance at ...
+    * normalsData : <class 'PyFFI.XmlHandler.DataStreamChunk'> instance at ...
+    * uvData : None
+    * colorsData : None
+    * colors2Data : None
+    * indicesData : <class 'PyFFI.XmlHandler.DataStreamChunk'> instance at ...
+    * tangentsData : None
+    * shCoeffsData : None
+    * shapeDeformationData : None
+    * boneMapData : None
+    * faceMapData : None
+    * vertMatsData : None
+    * reservedData :
+        <class 'PyFFI.Bases.Array.Array'> instance at ...
+        0: None
+        1: None
+        2: None
+        3: None
+    * physicsData :
+        <class 'PyFFI.Bases.Array.Array'> instance at ...
+        0: None
+        1: None
+        2: None
+        3: None
+    * minBound : [  0.000  0.000  0.000 ]
+    * maxBound : [  1.000  1.000  1.000 ]
+    * reserved3 :
+        <class 'PyFFI.Bases.Array.Array'> instance at ...
+        0: 0
+        1: 0
+        2: 0
+        3: 0
+        4: 0
+        5: 0
+        6: 0
+        7: 0
+        8: 0
+        9: 0
+        10: 0
+        11: 0
+        12: 0
+        13: 0
+        14: 0
+        15: 0
+        16: 0
+        etc...
+    <BLANKLINE>
+    >>> print chunk.meshSubsets # doctest: +ELLIPSIS
+    <class 'PyFFI.XmlHandler.MeshSubsetsChunk'> instance at ...
+    * flags :
+        <class 'PyFFI.XmlHandler.MeshSubsetsFlags'> instance at ...
+        * shHasDecomprMat : 0
+        * boneIndices : 0
+    * numMeshSubsets : 2
+    * reserved1 : 0
+    * reserved2 : 0
+    * meshSubsets :
+        <class 'PyFFI.Bases.Array.Array'> instance at ...
+        0: <class 'PyFFI.XmlHandler.MeshSubset'> instance at ...
+        * firstIndex : 0
+        * numIndices : 6
+        * firstVertex : 0
+        * numVertices : 4
+        * matId : 2
+        * radius : 0.707106769...
+        * center : [  0.500  0.500  0.000 ]
+        1: <class 'PyFFI.XmlHandler.MeshSubset'> instance at ...
+        * firstIndex : 6
+        * numIndices : 6
+        * firstVertex : 4
+        * numVertices : 4
+        * matId : 5
+        * radius : 0.707106769...
+        * center : [  0.500  0.500  1.000 ]
+    <BLANKLINE>
+    >>> print chunk.verticesData # doctest: +ELLIPSIS
+    <class 'PyFFI.XmlHandler.DataStreamChunk'> instance at ...
+    * flags : 0
+    * dataStreamType : VERTICES
+    * numElements : 8
+    * bytesPerElement : 12
+    * reserved1 : 0
+    * reserved2 : 0
+    * vertices :
+        <class 'PyFFI.Bases.Array.Array'> instance at ...
+        0: [  0.000  0.000  0.000 ]
+        1: [  0.000  1.000  0.000 ]
+        2: [  1.000  0.000  0.000 ]
+        3: [  1.000  1.000  0.000 ]
+        4: [  0.000  0.000  1.000 ]
+        5: [  0.000  1.000  1.000 ]
+        6: [  1.000  0.000  1.000 ]
+        7: [  1.000  1.000  1.000 ]
+    <BLANKLINE>
     """
     # get total number of vertices
     numvertices = sum(len(vertices) for vertices in verticeslist)
@@ -174,6 +354,8 @@ def setGeometry(self,
     self.faces.updateSize()
 
     # Crysis data preparation
+    self.numIndices = numtriangles * 3
+
     self.verticesData = self.cls.DataStreamChunk()
     self.verticesData.dataStreamType = self.cls.DataStreamType.VERTICES
     self.verticesData.bytesPerElement = 12
@@ -201,13 +383,12 @@ def setGeometry(self,
     if matlist is None:
         matlist = itertools.repeat(0)
     if uvslist is None:
-        uvs = itertools.repeat(None)
+        uvslist = itertools.repeat(None)
     if colorslist is None:
         colorslist = itertools.repeat(None)
 
     selfvertices_iter = iter(self.vertices)
     selfverticesData_iter = iter(self.verticesData.vertices)
-    selfnormals_iter = iter(self.normals)
     selfnormalsData_iter = iter(self.normalsData.normals)
     selffaces_iter = iter(self.faces)
 
@@ -226,13 +407,15 @@ def setGeometry(self,
         meshsubset.firstVertex = firstvertexindex
         meshsubset.numVertices = len(vertices)
         meshsubset.matId = mat
-        meshsubset.radius = 0 # TODO
-        meshsubset.center = 0 # TODO
+        center, radius = MathUtils.getCenterRadius(vertices)
+        meshsubset.radius = radius
+        meshsubset.center.x = center[0]
+        meshsubset.center.y = center[1]
+        meshsubset.center.z = center[2]
 
         # set vertex coordinates and normals for Far Cry
-        for cryvert, vert, norm in izip(selfvertices_iter,
-                                        vertices,
-                                        normals):
+        for vert, norm in izip(vertices, normals):
+            cryvert = selfvertices_iter.next()
             cryvert.p.x = vert[0]
             cryvert.p.y = vert[1]
             cryvert.p.z = vert[2]
@@ -241,10 +424,9 @@ def setGeometry(self,
             cryvert.n.z = norm[2]
 
         # set vertex coordinates and normals for Crysis
-        for cryvert, crynorm, vert, norm in izip(selfverticesData_iter,
-                                                 selfnormalsData_iter,
-                                                 vertices,
-                                                 normals):
+        for vert, norm in izip(vertices, normals):
+            cryvert = selfverticesData_iter.next()
+            crynorm = selfnormalsData_iter.next()
             cryvert.x = vert[0]
             cryvert.y = vert[1]
             cryvert.z = vert[2]
@@ -253,17 +435,32 @@ def setGeometry(self,
             crynorm.z = norm[2]
 
         # set Far Cry face info
-        for cryface, triangle in izip(selffaces_iter, triangles):
+        for triangle in triangles:
+            cryface = selffaces_iter.next()
             cryface.v0 = triangle[0] + firstvertexindex
             cryface.v1 = triangle[1] + firstvertexindex
             cryface.v2 = triangle[2] + firstvertexindex
             cryface.material = mat
 
         # set Crysis face info
-        for i, vertexindex in enumerate(itertools.chain(triangles)):
+        for i, vertexindex in enumerate(itertools.chain(*triangles)):
             self.indicesData.indices[i + firstindicesindex] \
                 = vertexindex + firstvertexindex
 
         # update index offsets
         firstvertexindex += len(vertices)
         firstindicesindex += 3 * len(triangles)
+
+    # set global bounding box
+    minbound, maxbound = MathUtils.getBoundingBox(
+        list(itertools.chain(*verticeslist)))
+    self.minBound.x = minbound[0]
+    self.minBound.y = minbound[1]
+    self.minBound.z = minbound[2]
+    self.maxBound.x = maxbound[0]
+    self.maxBound.y = maxbound[1]
+    self.maxBound.z = maxbound[2]
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
