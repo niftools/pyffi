c9f119202780efba968fe07873662e9bfe806b00
Amorilia <amorilia@users.sourceforge.net>
2008-03-22 13:43:38 +0000

    cgf: DataStreamChunk scaling fix

--- PyFFI/CGF/DataStreamChunk.py
+++ PyFFI/CGF/DataStreamChunk.py
@@ -42,6 +42,6 @@ def applyScale(self, scale):
     if abs(scale - 1.0) < self.cls.EPSILON:
         return
     for vert in self.vertices:
-        vert.p.x *= scale
-        vert.p.y *= scale
-        vert.p.z *= scale
+        vert.x *= scale
+        vert.y *= scale
+        vert.z *= scale
