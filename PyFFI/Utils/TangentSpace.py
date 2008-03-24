e350f5cece030d39c04628b5c48e6f6e90c27dc5
Amorilia <amorilia@users.sourceforge.net>
2008-03-24 11:59:10 +0000

    tangentspace: increase threshold for normal norm check

--- PyFFI/Utils/TangentSpace.py
+++ PyFFI/Utils/TangentSpace.py
@@ -121,9 +121,9 @@ def getTangentSpace(vertices = None, normals = None, uvs = None,
     xvec = (1, 0, 0)
     yvec = (0, 1, 0)
     for i, norm in enumerate(normals):
-        if abs(1-vecNorm(norm)) > 0.00001:
+        if abs(1-vecNorm(norm)) > 0.01:
             raise ValueError(
-                "tangentspace: unnormalized norm in list of norms (%s)" % norm)
+                "tangentspace: unnormalized normal in list of normals (%s, norm is %f)" % (norm, vecNorm(norm)))
         try:
             # turn norm, bin, tan into a base via Gram-Schmidt
             bin[i] = vecSub(bin[i],
