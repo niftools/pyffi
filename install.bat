8ea16865ffbad818254bde7e2354a7c285953c45
Amorilia <amorilia@users.sourceforge.net>
2008-03-29 12:26:22 +0000

    pyffi: quick install batch file for windows

---
+++ install.bat
@@ -0,0 +1,33 @@
+@rem quick pyffi install batch file
+
+@echo off
+
+rem find python
+set PYTHONPATH=
+for /f "tokens=3* delims=	 " %%A in ('reg.exe query "HKLM\SOFTWARE\Python\PythonCore\2.5\InstallPath\" \ve') do set PYTHONPATH="%%B"
+
+rem not found... complain and quit
+if not defined PYTHONPATH (
+echo.
+echo Python 2.5 not found!
+echo.
+goto end
+)
+
+rem python in registry but executable not found... complain and exit
+if not exist %PYTHONPATH%\python.exe (
+echo.
+echo python.exe not found!
+echo.
+goto end
+)
+
+rem run the installation from the setup script
+%PYTHONPATH%\python.exe setup.py install
+
+:end
+
+rem remove local variables
+set PYTHONPATH=
+
+pause
