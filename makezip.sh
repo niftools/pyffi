ddfc19a24ecc177c22ba2f91ebed1660ebfa3863
Amorilia <amorilia@users.sourceforge.net>
2008-04-07 00:27:01 +0000

    added bitmap for pyffi bdist_wininst installer

--- makezip.sh
+++ makezip.sh
@@ -3,5 +3,5 @@
 rm MANIFEST
 python setup.py sdist --format=zip
 python setup.py sdist --format=bztar
-python setup.py bdist_wininst --install-script pyffipostinstallation.py
+python setup.py bdist_wininst --install-script pyffipostinstallation.py --bitmap pyffi_install_152x261.bmp
 
