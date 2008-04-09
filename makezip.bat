346b32c08e4b257d406157b920cd68c40493d92f
Amorilia <amorilia@users.sourceforge.net>
2008-04-09 12:24:54 +0000

    pyffi: makezip.bat now finds python

--- makezip.bat
+++ makezip.bat
@@ -1,4 +1,25 @@
 del MANIFEST
-python setup.py sdist --format=zip
-python setup.py sdist --format=bztar
-python setup.py bdist_wininst --install-script pyffipostinstallation.py --bitmap pyffi_install_152x261.bmp
+
+rem find python
+set PYTHONPATH=
+for /f "tokens=3* delims=	 " %%A in ('reg.exe query "HKLM\SOFTWARE\Python\PythonCore\2.5\InstallPath\" \ve') do set PYTHONPATH="%%B"
+
+rem not found... complain and quit
+if not defined PYTHONPATH (
+echo.
+echo Python 2.5 not found!
+echo.
+goto end
+)
+
+rem python in registry but executable not found... complain and exit
+if not exist %PYTHONPATH%\python.exe (
+echo.
+echo python.exe not found!
+echo.
+goto end
+)
+
+%PYTHONPATH%\python.exe setup.py sdist --format=zip
+%PYTHONPATH%\python.exe setup.py sdist --format=bztar
+%PYTHONPATH%\python.exe setup.py bdist_wininst --install-script pyffipostinstallation.py --bitmap pyffi_install_152x261.bmp
