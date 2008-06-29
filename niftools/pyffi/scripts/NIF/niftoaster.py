e06ea007d712cf865800915c4b584e9135336e33
Amorilia <amorilia@users.sourceforge.net>
2008-06-29 15:45:57 +0000

    update niftoaster to new spell system

--- niftools/pyffi/scripts/NIF/niftoaster.py
+++ niftools/pyffi/scripts/NIF/niftoaster.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python
 
-"""A script for running hacking, surgery, and validation tests (based on wz's NifTester)
-
-For validate and surgery tests, exceptions during read will be raised.
-For hacking tests, exceptions during read will be passed; these are intended
-to hack .
-Unlike validate tests, surgery tests modify the original nif file.
-
-These three functions in the tester script are called:
-   testBlock(block) - will be called on every block in the nif
-   testRoot(root)   - will be called on every root block of the nif
-   testFile(version, user_version, f, roots)
-                    - will be called on every nif
-Not all of these three functions need to be present."""
+"""A script for casting spells on nif files. This script is essentially
+a nif specific wrapper around L{PyFFI.Spells.toaster}."""
 
 # --------------------------------------------------------------------------
 # ***** BEGIN LICENSE BLOCK *****
@@ -53,13 +42,12 @@ Not all of these three functions need to be present."""
 # ***** END LICENSE BLOCK *****
 # --------------------------------------------------------------------------
 
-import sys, os
-from optparse import OptionParser
+from PyFFI.Spells import toaster
+from PyFFI.Formats.NIF import NifFormat
+import PyFFI.Spells.NIF
 
-import NifTester
-
-def examples_callback(option, opt, value, parser):
-    print """* check if the library can read all files in current directory
+def main():
+    examples = """* check if PyFFI can read all files in current directory
   (python version of nifskope's xml checker):
 
     python niftoaster.py read .
@@ -82,87 +70,11 @@ def examples_callback(option, opt, value, parser):
 * find out time spent on a particular test:
 
     python -m cProfile -s cumulative niftoaster.py tristrip"""
-    sys.exit(0)
 
-def tests_callback(option, opt, value, parser):
-    for category in ('hacking', 'surgery', 'validate'):
-        print category + ':'
-        tests = __import__('NifTester.' + category)
-        tests = getattr(tests, category)
-        for test in dir(tests):
-            if test[:2] == '__': continue
-            print '  ' + test
-    sys.exit(0)
-
-def main():
-    # parse options and positional arguments
-    usage = "%prog [options] <tester> <file>|<folder>"
-    description="""Look for a python script "NifTester.hacking.<tester>",
-"NifTester.validate.<tester>", or "NifTester.surgery.<tester>"
-and use the functions testRoot, testBlock, and testFile therein
-for hacking, modifying, or validating <file>, or the files in <folder>."""
-
-    parser = OptionParser(usage, version="%prog $Rev$", description=description)
-    parser.add_option("-a", "--arg", dest="arg",
-                      type="string",
-                      metavar="ARG",
-                      help="pass argument ARG to tester")
-    parser.add_option("--examples",
-                      action="callback", callback=examples_callback,
-                      help="show examples of usage and exit")
-    parser.add_option("-v", "--verbose", dest="verbose",
-                      type="int",
-                      metavar="VERBOSE",
-                      default=1,
-                      help="verbosity level: 0, 1, or 2 [default: %default]")
-    parser.add_option("--testers",
-                      action="callback", callback=tests_callback,
-                      help="list all testers and exit")
-    # TODO
-##    parser.add_option("-f", "--filename", dest="filename",
-##                      metavar="FILE",
-##                      help="write output to FILE instead of to the terminal")
-    (options, args) = parser.parse_args()
-
-    if len(args) != 2:
-        parser.error("incorrect number of arguments (two required)")
-
-    # get tester and top folder/file
-    test_str = args[0]
-    top = args[1]
-
-    try:
-        testers = __import__('NifTester.hacking.' + test_str)
-        test = getattr(testers.hacking, test_str)
-        raisereaderror = False
-        mode = 'rb'
-    except ImportError:
-        try:
-            testers = __import__('NifTester.validate.' + test_str)
-            test = getattr(testers.validate, test_str)
-            raisereaderror = True
-            mode = 'rb'
-        except ImportError:
-            try:
-                testers = __import__('NifTester.surgery.' + test_str)
-                test = getattr(testers.surgery, test_str)
-                raisereaderror = True
-                mode = 'r+b'
-            except ImportError:
-                # either tester was not found, or had an error while importing
-                parser.error("tester '%s' not found" % test_str)
-
-    testBlock = getattr(test, 'testBlock', None)
-    testRoot = getattr(test, 'testRoot', None)
-    testFile = getattr(test, 'testFile',
-                       NifTester.testFileOverwrite if mode == 'r+b' else None)
-
-    # run tester
-    NifTester.testPath(
-        top,
-        testBlock = testBlock, testRoot = testRoot, testFile = testFile,
-        raisereaderror = raisereaderror, mode = mode, verbose = options.verbose,
-        arg = options.arg)
+    toaster(ext='NIF',
+            format=NifFormat,
+            formatspellsmodule=PyFFI.Spells.NIF,
+            examples=examples)
 
 # if script is called...
 if __name__ == "__main__":
