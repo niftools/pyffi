c2c605b0502ed12d34b92c3b5e5cf67923d8120f
Amorilia <amorilia@users.sourceforge.net>
2008-07-03 13:36:01 +0000

    nifdump to dump

--- niftools/pyffi/PyFFI/Spells/NIF/dump.py
+++ niftools/pyffi/PyFFI/Spells/NIF/dump.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-
-"""A script for dumping particular blocks from nifs."""
+"""A spell for dumping particular blocks from nifs."""
 
 # --------------------------------------------------------------------------
 # ***** BEGIN LICENSE BLOCK *****
@@ -41,24 +39,25 @@
 # ***** END LICENSE BLOCK *****
 # --------------------------------------------------------------------------
 
-import NifTester
+from PyFFI.Formats.NIF import NifFormat
 
-############################################################################
-# custom functions
-############################################################################
+def dumpArray(arr):
+    """Format an array.
 
-def dumpArray(block):
-    """Dump an array."""
+    @param arr: An array.
+    @type arr: L{PyFFI.Bases.Array.Array}
+    @return: String describing the array.
+    """
     text = ""
-    if block._count2 == None:
-        for i, element in enumerate(list.__iter__(block)):
+    if arr._count2 == None:
+        for i, element in enumerate(list.__iter__(arr)):
             if i > 16:
                 text += "etc...\n"
                 break
             text += "%i: %s\n" % (i, dumpAttr(element))
     else:
         k = 0
-        for i, elemlist in enumerate(list.__iter__(block)):
+        for i, elemlist in enumerate(list.__iter__(arr)):
             for j, elem in enumerate(list.__iter__(elemlist)):
                 if k > 16:
                     text += "etc...\n"
@@ -70,10 +69,16 @@ def dumpArray(block):
     return text if text else "None"
 
 def dumpBlock(block):
-    """Print block without following references."""
+    """Return formatted string for block without following references.
+
+    @param block: The block to print.
+    @type block: L{NifFormat.NiObject}
+    @return: String string describing the block.
+    """
     text = '%s instance at 0x%08X\n' % (block.__class__.__name__, id(block))
     for attr in block._filteredAttributeList():
-        attr_str_lines = dumpAttr(getattr(block, "_%s_value_" % attr.name)).splitlines()
+        attr_str_lines = \
+            dumpAttr(getattr(block, "_%s_value_" % attr.name)).splitlines()
         if len(attr_str_lines) > 1:
             text += '* %s :\n' % attr.name
             for attr_str in attr_str_lines:
@@ -83,10 +88,17 @@ def dumpBlock(block):
     return text
 
 def dumpAttr(attr):
+    """Format an attribute.
+
+    @param attr: The attribute to print.
+    @type attr: (anything goes)
+    @return: String for the attribute.
+    """
     if isinstance(attr, (NifFormat.Ref, NifFormat.Ptr)):
         ref = attr.getValue()
         if ref:
-            return "<%s instance at 0x%08X>" % (ref.__class__.__name__, id(attr))
+            return "<%s instance at 0x%08X>" % (ref.__class__.__name__,
+                                                id(attr))
         else:
             return "<None>"
     elif isinstance(attr, list):
@@ -96,66 +108,11 @@ def dumpAttr(attr):
     else:
         return str(attr)
 
-############################################################################
-# testers
-############################################################################
-
-# set this variable to True for scripts that need to overwrite of original files
-OVERWRITE_FILES = False
-
 def testBlock(block, **args):
-    if isinstance(block, getattr(NifFormat, args["blocktype"])):
-        print dumpBlock(block)
+    """Print block.
 
-############################################################################
-# main program
-############################################################################
+    @param block: The block to print.
+    @type block: L{NifFormat.NiObject}
+    """
+    print dumpBlock(block)
 
-import sys, os
-from optparse import OptionParser
-
-from PyFFI.Formats.NIF import NifFormat
-
-def main():
-    # parse options and positional arguments
-    usage = "%prog [options] <file>|<folder>"
-    description="""A nif script for parsing nif file <file> or all nif files in folder
-<folder> and dumping particular blocks."""
-    if OVERWRITE_FILES:
-        description += """
-WARNING:
-This script will modify the nif files, in particular if something goes wrong it
-may destroy them. Make a backup before running this script."""
-    parser = OptionParser(usage, version="%prog $Rev$", description=description)
-    parser.add_option("-v", "--verbose", dest="verbose",
-                      type="int",
-                      metavar="VERBOSE",
-                      default=1,
-                      help="verbosity level: 0, 1, or 2 [default: %default]")
-    parser.add_option("-b", "--blocktype", dest="blocktype",
-                      type="string",
-                      metavar="BLOCKTYPE",
-                      default="NiNode",
-                      help="block type [default: %default]")
-    (options, args) = parser.parse_args()
-
-    if len(args) != 1:
-        parser.error("incorrect number of arguments (one required)")
-
-    # get top folder/file
-    top = args[0]
-
-    # warning
-    if OVERWRITE_FILES:
-        print """This script will modify the nif files, in particular if something goes wrong it
-may destroy them. Make a backup of your nif files before running this script.
-"""
-        if raw_input("Are you sure that you want to proceed? [n/Y] ") != "Y": return
-
-    # run tester
-    mode = "rb" if not OVERWRITE_FILES else "r+b"
-    NifTester.testPath(top, testBlock = testBlock, raisereaderror = True, mode = mode, verbose = options.verbose, blocktype = options.blocktype)
-
-# if script is called...
-if __name__ == "__main__":
-    main()
