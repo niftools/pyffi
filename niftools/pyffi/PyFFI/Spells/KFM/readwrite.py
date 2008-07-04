7cf88fb92d37fbb23d7d989266257d3759e3692f
Amorilia <amorilia@users.sourceforge.net>
2008-07-04 10:03:21 +0000

    update for readwrite tests

--- niftools/pyffi/PyFFI/Spells/KFM/readwrite.py
+++ niftools/pyffi/PyFFI/Spells/KFM/readwrite.py
@@ -1,10 +1,27 @@
-# writes back the file and raises StandardError if sizes differ
+"""Spell for testing the writing of kfm files."""
+
+from tempfile import TemporaryFile
 
 from PyFFI.Formats.KFM import KfmFormat
-from tempfile import TemporaryFile
 
 def testFile(stream, version = None, user_version = None,
              header = None, animations = None, footer = None, **args):
+    """Write back the file to a temporary file and raises StandardError if
+    sizes differ.
+
+    @param stream: The parsed file.
+    @type stream: file
+    @param version: The version of the parsed file.
+    @type version: int
+    @param user_version: The user version of the parsed file.
+    @type user_version: int
+    @param header: The kfm header.
+    @type header: L{KfmFormat.Header}
+    @param animations: The animation data.
+    @type animations: list of L{KfmFormat.Animation}
+    @param footer: The kfm footer.
+    @type footer: L{KfmFormat.Footer}
+    """
     f_tmp = TemporaryFile()
     try:
         KfmFormat.write(
@@ -13,8 +30,8 @@ def testFile(stream, version = None, user_version = None,
             header = header, animations = animations, footer = footer)
         # cheaply just compare file sizes
 
-        stream.seek(0,2)
-        f_tmp.seek(0,2)
+        stream.seek(0, 2)
+        f_tmp.seek(0, 2)
         if stream.tell() != f_tmp.tell():
             print "original size: %i" % (stream.tell())
             print "written size:  %i" % (f_tmp.tell())
@@ -25,3 +42,4 @@ def testFile(stream, version = None, user_version = None,
             raise StandardError('write check failed: file sizes differ')
     finally:
         f_tmp.close()
+
--- niftools/pyffi/scripts/KFM/kfmtoaster.py
+++ niftools/pyffi/scripts/KFM/kfmtoaster.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python
 
-"""A script for running hacking, surgery, and validation tests.
-
-For validate and surgery tests, exceptions during read will be raised.
-For hacking tests, exceptions during read will be passed; these are intended
-to hack.
-Unlike validate tests, surgery tests modify the original kfm file.
-
-This function in the tester script are called:
-   testFile(version, f, kfm)
-                    - will be called on every kfm
-Not all of these three functions need to be present."""
+"""A script for casting spells on kfm files. This script is essentially
+a kfm specific wrapper around L{PyFFI.Spells.toaster}."""
 
 # --------------------------------------------------------------------------
 # ***** BEGIN LICENSE BLOCK *****
@@ -51,95 +42,17 @@ Not all of these three functions need to be present."""
 # ***** END LICENSE BLOCK *****
 # --------------------------------------------------------------------------
 
-import sys, os
-from optparse import OptionParser
+from PyFFI.Spells import toaster
+from PyFFI.Formats.KFM import KfmFormat
+import PyFFI.Spells.KFM
 
-import KfmTester
-
-def examples_callback(option, opt, value, parser):
-    print """* check if the library can read all files in current directory:
+def main():
+    examples = """* check if the library can read all files in current directory:
 
     python kfmtoaster.py read ."""
 
-    sys.exit(0)
-
-def tests_callback(option, opt, value, parser):
-    for category in ('hacking', 'surgery', 'validate'):
-        print category + ':'
-        tests = __import__('KfmTester.' + category)
-        tests = getattr(tests, category)
-        for test in dir(tests):
-            if test[:2] == '__': continue
-            print '  ' + test
-    sys.exit(0)
-
-def main():
-    # parse options and positional arguments
-    usage = "%prog [options] <tester> <file>|<folder>"
-    description="""Look for a python script "KfmTester.hacking.<tester>",
-"KfmTester.validate.<tester>", or "KfmTester.surgery.<tester>"
-and use the functions testRoot, testBlock, and testFile therein
-for hacking, modifying, or validating <file>, or the files in <folder>."""
-
-    parser = OptionParser(usage, version="%prog $Rev$", description=description)
-    parser.add_option("-a", "--arg", dest="arg",
-                      type="string",
-                      metavar="ARG",
-                      help="pass argument ARG to tester")
-    parser.add_option("--examples",
-                      action="callback", callback=examples_callback,
-                      help="show examples of usage and exit")
-    parser.add_option("-v", "--verbose", dest="verbose",
-                      type="int",
-                      metavar="VERBOSE",
-                      default=1,
-                      help="verbosity level: 0, 1, or 2 [default: %default]")
-    parser.add_option("--testers",
-                      action="callback", callback=tests_callback,
-                      help="list all testers and exit")
-    # TODO
-##    parser.add_option("-f", "--filename", dest="filename",
-##                      metavar="FILE",
-##                      help="write output to FILE instead of to the terminal")
-    (options, args) = parser.parse_args()
-
-    if len(args) != 2:
-        parser.error("incorrect number of arguments (two required)")
-
-    # get tester and top folder/file
-    test_str = args[0]
-    top = args[1]
-
-    try:
-        testers = __import__('KfmTester.hacking.' + test_str)
-        test = getattr(testers.hacking, test_str)
-        raisereaderror = False
-        mode = 'rb'
-    except ImportError:
-        try:
-            testers = __import__('KfmTester.validate.' + test_str)
-            test = getattr(testers.validate, test_str)
-            raisereaderror = True
-            mode = 'rb'
-        except ImportError:
-            try:
-                testers = __import__('KfmTester.surgery.' + test_str)
-                test = getattr(testers.surgery, test_str)
-                raisereaderror = True
-                mode = 'r+b'
-            except ImportError:
-                # either tester was not found, or had an error while importing
-                parser.error("tester '%s' not found" % test_str)
-
-    testFile = getattr(test, 'testFile',
-                       KfmTester.testFileOverwrite if mode == 'r+b' else None)
-
-    # run tester
-    KfmTester.testPath(
-        top,
-        testFile = testFile,
-        raisereaderror = raisereaderror, mode = mode, verbose = options.verbose,
-        arg = options.arg)
+    toaster(format=KfmFormat, formatspellsmodule=PyFFI.Spells.KFM,
+            examples=examples)
 
 # if script is called...
 if __name__ == "__main__":
