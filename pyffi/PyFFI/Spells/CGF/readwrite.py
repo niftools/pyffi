ed9e0d1c2fd7f21f1e7280d5eae2e4809d184119
Amorilia <amorilia@users.sourceforge.net>
2008-07-04 10:02:46 +0000

    update for readwrite tests

--- pyffi/PyFFI/Spells/CGF/readwrite.py
+++ pyffi/PyFFI/Spells/CGF/readwrite.py
@@ -1,4 +1,4 @@
-"""This spell writes back the file and raises StandardError if sizes differ."""
+"""Spell for testing the writing of cgf files."""
 
 from tempfile import TemporaryFile
 
@@ -7,6 +7,25 @@ from PyFFI.Formats.CGF import CgfFormat
 def testFile(stream,
              version = None, user_version = None,
              filetype = None, chunks = None, versions = None, **kwargs):
+    """Writes back the file to a temporary file and raises StandardError if
+    sizes differ.
+
+    @param stream: The parsed file.
+    @type stream: file
+    @param version: The version number.
+    @type version: int
+    @param user_version: The user version number.
+    @type user_version: int
+    @param filetype: The file type, L{FileType.GEOM} or L{FileType.ANIM}.
+    @type filetype: int
+    @param chunks: The file chunks.
+    @type chunks: list of L{CgfFormat.Chunk}s
+    @param versions: The file chunk versions. If C{None} then these will
+        be automatically obtained through L{getChunkVersions}.
+    @type versions: list of ints
+    @param verbose: The level of verbosity.
+    @type verbose: int
+    """
     verbose = kwargs.get("verbose")
     f_tmp = TemporaryFile()
     try:
