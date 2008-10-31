83b0a488af2e36b3c305f589a371ccf560a2bf42
Amorilia <amorilia@users.sourceforge.net>
2008-10-31 10:00:08 +0000

    pyffi: fixed a doctest

--- niftools/pyffi/PyFFI/Formats/NIF/__init__.py
+++ niftools/pyffi/PyFFI/Formats/NIF/__init__.py
@@ -56,6 +56,7 @@ reading tests/nif/invalid.nif
 Warning: read failed due corrupt file, corrupt format description, or bug.
 reading tests/nif/test.nif
 reading tests/nif/test_dump_tex.nif
+reading tests/nif/test_fix_detachhavoktristripsdata.nif
 reading tests/nif/test_fix_ffvt3rskinpartition.nif
 reading tests/nif/test_fix_tangentspace.nif
 reading tests/nif/test_fix_texturepath.nif
