a943712ee4ba6c6da91d6114a07662ac989a872f
Amorilia <amorilia@users.sourceforge.net>
2008-11-13 16:34:16 +0000

    fix issue with epydoc getting confused with imports

--- pyffi/makezip.sh
+++ pyffi/makezip.sh
@@ -3,7 +3,9 @@
 # generate documentation
 
 rm -rfv docs
-epydoc -v --output=docs --name='Python File Format Interface' --url='http://pyffi.sourceforge.net/' --navlink='&nbsp;&nbsp;&nbsp;<a class="navbar" target="_top" href="http://pyffi.sourceforge.net/">Python File Format Interface</a>&nbsp;&nbsp;&nbsp;</th><th class="navbar" align="center">&nbsp;&nbsp;&nbsp;<a class="navbar" target="_top" href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=199269" width="88" height="31" border="0" alt="SourceForge.net Logo" /></a>&nbsp;&nbsp;&nbsp;' --top=PyFFI PyFFI
+cd ..
+epydoc -v --output=pyffi/docs --name='Python File Format Interface' --url='http://pyffi.sourceforge.net/' --navlink='&nbsp;&nbsp;&nbsp;<a class="navbar" target="_top" href="http://pyffi.sourceforge.net/">Python File Format Interface</a>&nbsp;&nbsp;&nbsp;</th><th class="navbar" align="center">&nbsp;&nbsp;&nbsp;<a class="navbar" target="_top" href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=199269" width="88" height="31" border="0" alt="SourceForge.net Logo" /></a>&nbsp;&nbsp;&nbsp;' --top=PyFFI PyFFI
+cd pyffi
 
 # create source and binary distributions
 
