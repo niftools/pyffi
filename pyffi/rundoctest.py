d5e42f23da07494e6b638d39b94c0f4b994639de
Amorilia <amorilia@users.sourceforge.net>
2008-11-13 16:52:37 +0000

    more doctests

--- pyffi/rundoctest.py
+++ pyffi/rundoctest.py
@@ -22,7 +22,11 @@ import PyFFI.Utils.TangentSpace
 import PyFFI.Utils.Mopp
 import PyFFI.Utils.Partial
 import PyFFI.Formats.NIF
+import PyFFI.Formats.NIF._NifFormat
 import PyFFI.Formats.NIF._Header
+import PyFFI.Formats.NIF._Matrix33
+import PyFFI.Formats.NIF._Vector3
+import PyFFI.Formats.NIF._Vector4
 import PyFFI.Formats.NIF.NiGeometryData
 import PyFFI.Formats.NIF.NiTriStripsData
 import PyFFI.Formats.NIF.NiTriShapeData
