0fa8a84e51d553ef64153b2cac5e1d32f06e8547
Amorilia <amorilia@users.sourceforge.net>
2009-03-13 16:26:17 +0000

    pyffi: install script sets mopper as executable

--- pyffi/cleaninstall.sh
+++ pyffi/cleaninstall.sh
@@ -4,4 +4,5 @@ rm -rf MANIFEST build dist docs PyFFI-* win-install/manifest.nsh
 python setup.py -q build
 su -c 'rm -rf /usr/lib/python2.5/site-packages/PyFFI*; rm -rf /usr/lib64/python2.5/site-packages/PyFFI*; python setup.py -q install'
 rm -rf build
+chmod +x /usr/lib/python2.5/site-packages/PyFFI/Utils/mopper.exe
 
