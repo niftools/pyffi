3851551fb835c8a711725ea4e205b596ad2cf5bc
Amorilia <amorilia@users.sourceforge.net>
2009-03-30 13:09:13 +0000

    pyffi: updated the SimpleType object model implementation (let value be a property, this avoids having to define new types and makes future Pyrex/Cython implementation of simple types easier)

--- pyffi/PyFFI/ObjectModels/SimpleType.py
+++ pyffi/PyFFI/ObjectModels/SimpleType.py
@@ -47,77 +47,65 @@ class SimpleType(PyFFI.ObjectModels.AnyType.AnyType):
     and that can represented efficiently by a (usually native) Python type,
     typically C{int}, C{float}, or L{str}.
 
-    When overriding this class, set L{ValueType} to the type used to store
-    this data. If it is a native type but requires extra validation
-    (for example if not all Python values are admissible), derive a wrapper
-    class around the native type and place an extra check in C{__new__}.
+    A brief example of usage (without type checking):
+
+    >>> class Int(SimpleType):
+    ...     def __init__(self, value=None):
+    ...         self.value = 0 if value is None else value
+    >>> print(Int(value=12345))
+    12345
+
+    A slightly more complicated example, demonstrating how to implement
+    type checking:
+
+    >>> class Short(SimpleType):
+    ...     def __init__(self, value=None):
+    ...         # for fun, let default value be 3
+    ...         self.value = 3 if value is None else value
+    ...     def _get(self):
+    ...         return self._value
+    ...     def _set(self, value):
+    ...         # check type
+    ...         if not isinstance(value, int):
+    ...             raise TypeError("Expected int but got %s."
+    ...                             % value.__class__.__name__)
+    ...         # check range
+    ...         if value < -0x8000 or value > 0x7fff:
+    ...             raise ValueError("Value %i out of range." % value)
+    ...         self._value = value
+    ...     value = property(_get, _set)
+    >>> print(Short(value=15))
+    15
+    >>> test = Short()
+    >>> print(test)
+    3
+    >>> test.value = 255
+    >>> print(test)
+    255
+    >>> test.value = 100000 # doctest: +ELLIPSIS
+    Traceback (most recent call last):
+        ...
+    ValueError: ...
+    >>> test.value = "hello world" # doctest: +ELLIPSIS
+    Traceback (most recent call last):
+        ...
+    TypeError: ...
 
     Also override L{read} and L{write} if you wish to read and write data
     of this type, and L{isInterchangeable} if you wish to declare data as
     equivalent.
 
-    A brief example of usage:
-
-    >>> class Int(SimpleType):
-    ...     ValueType = int
-    >>> print(Int(value=12345))
-    12345
-
-    A slightly more complicated example, demonstrating how to implement
-    custom types that derive from built-in types:
-
-    >>> class shortint(int):
-    ...     def __new__(cls, value=None):
-    ...         # for the sake of example, default value is 3
-    ...         val = int(value) if value is not None else 3
-    ...         if val < -0x8000 or val > 0x7fff:
-    ...             raise ValueError("value %i out of range" % val)
-    ...         return super(shortint, cls).__new__(cls, val)
-    >>> class Short(SimpleType):
-    ...     ValueType = shortint
-    >>> test = Short()
-    >>> print(test)
-    3
-    >>> test.value = test.ValueType(255)
-    >>> print(test)
-    255
-    >>> Short(value=100000) # doctest: +ELLIPSIS
-    Traceback (most recent call last):
-        ...
-    ValueError: ...
-    >>> print(Short(value=15))
-    15
-
-    @warning: When assigning to L{value}, it is good practice always to use
-        the L{ValueType} class, as in
-
-            >>> some.value = some.ValueType(somevalue) # doctest: +SKIP
-
-        This ensures that the value you assign is indeed of type
-        L{ValueType} and has an admissible value. If this is violated, you may
-        encounter hard to debug errors.
-
-    @cvar ValueType: The type used to store the data.
-    @type ValueType: C{type}
     @ivar value: The actual data.
-    @type value: L{ValueType}
+    @type value: C{type(None)}, or anything of the appropriate type.
     """
-    # value type
-    ValueType = type(None)
 
     def __init__(self, value=None):
         """Initialize the type with given value.
 
-        @keyword value: The initial value of the object. If not
-            C{None}, this value is passed as an argument to
-            L{ValueType}.
-        @type value: C{type(None)}, or anything acceptable as a first
-            argument to the L{ValueType} constructor.
+        @keyword value: The initial value of the object.
+        @type value: C{type(None)}, or anything of the appropriate type.
         """
-        if value is None:
-            self.value = self.ValueType()
-        else:
-            self.value = self.ValueType(value)
+        self.value = value
 
     def __str__(self):
         """String representation. This implementation is simply a wrapper
