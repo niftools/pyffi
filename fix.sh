660b3008eb4643b74d288d6e86da61d71e9601ae
Amorilia <amorilia@users.sourceforge.net>
2009-04-14 22:00:14 +0100

    Converted some remaining CRLF text files to LF.
    * All text files now use LF as far as I know.
    * Also this change should help msysGit users on Windows, where all line endings are committed back to the repository in LF by default.

--- fix.sh
+++ fix.sh
@@ -2,11 +2,7 @@
 
 # Automatically fix eol-style and executable flag on files.
 
-find . \( -name "Makefile" -or -name "*.py" -or -name "*.pyx" -or -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" -or -name "*.nsi" -or -name "*.nsh" -or -name "*.sh" -or -name "*.xml" -or -name "*.txt" -or -name "*.rst" \) -exec dos2unix {} \;
-
-# must first do dos2unix, to revert mixed eol-style
-find . \( -name "*.TXT" -or -name "*.bat" \) -exec dos2unix {} \;
-find . \( -name "*.TXT" -or -name "*.bat" \) -exec unix2dos {} \;
+find . \( -name "Makefile" -or -name "*.py" -or -name "*.pyx" -or -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" -or -name "*.nsi" -or -name "*.nsh" -or -name "*.sh" -or -name "*.xml" -or -name "*.txt" -or -name "*.rst" -or -name "*.TXT" -or -name "*.bat" \) -exec dos2unix {} \;
 
 find . \( -name "*.exe" -or -name "*.bat" -or -name "*.sh" \) -exec chmod +x {} \;
 
