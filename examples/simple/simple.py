4bc9278e9c809537a663627369bd444ad445808c
Amorilia <amorilia@users.sourceforge.net>
2009-04-25 20:46:51 +0100

    Another update to the simple example.

--- examples/simple/simple.py
+++ examples/simple/simple.py
@@ -2,15 +2,14 @@ import os
 import PyFFI.ObjectModels.XML.FileFormat
 import PyFFI.ObjectModels.Common
 
-class _SimpleFormat(PyFFI.ObjectModels.XML.FileFormat.XmlFileFormat):
+class SimpleFormat(PyFFI.ObjectModels.XML.FileFormat.XmlFileFormat):
     xmlFileName = 'simple.xml'
     xmlFilePath = [ os.path.dirname(__file__) ]
     clsFilePath = os.path.dirname(__file__)
 
     Int = PyFFI.ObjectModels.Common.Int
 
-class SimpleFormat(_SimpleFormat):
-    class Example(_SimpleFormat.Example):
+    class Example:
         def addInteger(self, x):
             self.numIntegers += 1
             self.integers.updateSize()
