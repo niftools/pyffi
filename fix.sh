a22365a3ff4f13010473413505d68b0968a3691a
Amorilia <amorilia@users.sourceforge.net>
2009-04-26 16:35:00 +0100

    Eol fix for Crytek .mtl files.

--- fix.sh
+++ fix.sh
@@ -2,7 +2,7 @@
 
 # Automatically fix eol-style and executable flag on files.
 
-find . \( -name "Makefile" -or -name "*.py" -or -name "*.pyx" -or -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" -or -name "*.nsi" -or -name "*.nsh" -or -name "*.sh" -or -name "*.xml" -or -name "*.txt" -or -name "*.rst" -or -name "*.TXT" -or -name "*.bat" \) -exec dos2unix {} \;
+find . \( -name "Makefile" -or -name "*.py" -or -name "*.pyx" -or -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.hpp" -or -name "*.nsi" -or -name "*.nsh" -or -name "*.sh" -or -name "*.xml" -or -name "*.txt" -or -name "*.rst" -or -name "*.TXT" -or -name "*.bat" -or -name "*.mtl" \) -exec dos2unix {} \;
 
 find . \( -name "*.exe" -or -name "*.bat" -or -name "*.sh" \) -exec chmod +x {} \;
 
