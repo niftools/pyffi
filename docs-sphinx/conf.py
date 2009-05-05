82667ff481b11e035add0820f85549c8b076de22
Amorilia <amorilia@users.sourceforge.net>
2009-05-05 22:06:32 +0100

    Removed unused customizations from Sphinx conf.py.

--- docs-sphinx/conf.py
+++ docs-sphinx/conf.py
@@ -163,18 +163,4 @@ latex_documents = [
 # If false, no module index is generated.
 #latex_use_modindex = True
 
-# customization
-
-def epydoctorest(app, what, name, obj, options, lines):
-    """Convert epydoc string into rest string."""
-    result = []
-    for line in lines:
-        line = line.replace("L{", "`")
-        line = line.replace("}", "`")
-        result.append(line)
-    return result
-
-#def setup(app):
-#    app.connect('autodoc-process-docstring', epydoctorest)
-
 
