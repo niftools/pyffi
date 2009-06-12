d6e5e0523056eb1a94c44dc4d68eec1fcee04bf2
Amorilia <amorilia@users.sourceforge.net>
2009-06-12 08:52:27 +0100

    The cleaninstall script now assumes Python 2.6 (for Fedora 11).

--- cleaninstall.sh
+++ cleaninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clean -f -d -x
 python setup.py -q build
-su -c 'rm -rf /usr/lib/python2.5/site-packages/PyFFI*; rm -rf /usr/lib64/python2.5/site-packages/PyFFI*; rm -rf /usr/lib/python2.5/site-packages/pyffi*; rm -rf /usr/lib64/python2.5/site-packages/pyffi*; python setup.py -q install; chmod +x /usr/lib/python2.5/site-packages/pyffi/utils/mopper.exe'
+su -c 'rm -rf /usr/lib/python2.6/site-packages/PyFFI*; rm -rf /usr/lib64/python2.6/site-packages/PyFFI*; rm -rf /usr/lib/python2.6/site-packages/pyffi*; rm -rf /usr/lib64/python2.6/site-packages/pyffi*; python setup.py -q install; chmod +x /usr/lib/python2.6/site-packages/pyffi/utils/mopper.exe'
 rm -rf build
 
