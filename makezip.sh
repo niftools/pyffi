27d112285323ca8be6b2c529a8300eb17be16b50
Amorilia <amorilia@users.sourceforge.net>
2009-08-10 12:07:17 +0100

    Include maya and blender sections in installer.

--- makezip.sh
+++ makezip.sh
@@ -37,5 +37,5 @@ cd ..
 rm MANIFEST
 python setup.py -q sdist --format=zip
 python setup.py -q sdist --format=bztar
-python setup.py --command-packages bdist_nsi bdist_nsi --bitmap=win-install/pyffi_install_164x314.bmp --headerbitmap=win-install/pyffi_install_150x57.bmp --run2to3 --msvc2008sp1 --nshextra=win-install/pyffi.nsh --target-versions=2.5,2.6,2.7,3.0,3.1,3.2
+python setup.py --command-packages bdist_nsi bdist_nsi --bitmap=win-install/pyffi_install_164x314.bmp --headerbitmap=win-install/pyffi_install_150x57.bmp --run2to3 --msvc2008sp1 --nshextra=win-install/pyffi.nsh --target-versions=2.5,2.6,2.7,3.0,3.1,3.2 --maya --blender
 
