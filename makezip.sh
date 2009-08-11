7dc29542aa29bf80a948451ce2538d444b8e3498
Amorilia <amorilia@users.sourceforge.net>
2009-08-11 11:23:36 +0100

    Added instructions to create exe with wine (avoids issues with system.dll in makensis on fedora 11).

--- makezip.sh
+++ makezip.sh
@@ -18,3 +18,7 @@ python setup.py -q sdist --format=zip
 python setup.py -q sdist --format=bztar
 python setup.py --command-packages bdist_nsi bdist_nsi --bitmap=win-install/pyffi_install_164x314.bmp --headerbitmap=win-install/pyffi_install_150x57.bmp --run2to3 --msvc2008sp1 --nshextra=win-install/pyffi.nsh --target-versions=2.5,2.6,2.7,3.0,3.1,3.2 --maya --blender
 
+# to build with wine: add the -k flag to keep all installer files, and run:
+#cd build/bdist.linux-x86_64/nsi
+#cat setup.nsi | sed 's/\\home\\/Z:\\home\\/g' | sed 's/\\usr\\/Z:\\usr\\/g' | wine ~/.wine/drive_c/Program\ Files/NSIS/makensis.exe -
+
