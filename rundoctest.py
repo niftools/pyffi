cccca4c0de4b1f4261ceee6a65981e3ad5a1fe38
Amorilia <amorilia@users.sourceforge.net>
2009-12-19 15:17:21 +0000

    Doctest for modify_delbranches.

--- rundoctest.py
+++ rundoctest.py
@@ -55,6 +55,7 @@ suite.addTest(doctest.DocFileSuite('tests/nif/fix_tangentspace_series_parallel.t
 suite.addTest(doctest.DocFileSuite('tests/nif/fix_detachhavoktristripsdata.txt'))
 suite.addTest(doctest.DocFileSuite('tests/nif/fix_clampmaterialalpha.txt'))
 suite.addTest(doctest.DocFileSuite('tests/nif/opt_mergeduplicates.txt'))
+suite.addTest(doctest.DocFileSuite('tests/nif/modify_delbranches.txt'))
 suite.addTest(doctest.DocFileSuite('tests/om_simpletype.txt'))
 suite.addTest(doctest.DocFileSuite('tests/om_arraytype.txt'))
 suite.addTest(doctest.DocFileSuite('tests/nif/matrix.txt'))
