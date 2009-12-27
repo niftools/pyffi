1579e3b4953de2cafdf9c3064dd1c2637d827f38
Amorilia <amorilia@users.sourceforge.net>
2009-12-27 17:43:00 +0000

    makepy3k.sh script fix, always use py3k to run 2to3.

--- makepy3k.sh
+++ makepy3k.sh
@@ -2,9 +2,7 @@
 
 git clean -x -d -f
 mkdir -p py3k
-cp -r pyffi scripts tests rundoctest.py py3k
-#2to3 -w py3k/pyffi py3k/rundoctest.py
-# better, if Python 3.x is available:
+cp -r pyffi scripts tests rundoctest.py docs-sphinx py3k
 cd py3k
 ../../Python-3.1.1/python ../../Python-3.1.1/Tools/scripts/2to3 -w -n pyffi rundoctest.py
 #../../Python-3.1.1/python rundoctest.py
