a2511d26d95e74d3b629d5473a52b11ab5fab68a
Amorilia <amorilia@users.sourceforge.net>
2009-12-28 12:25:54 +0000

    Improved WeakSet implementation.

--- pyffi/utils/trianglemesh.py
+++ pyffi/utils/trianglemesh.py
@@ -66,12 +66,13 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 import operator # itemgetter
-import weakref
+
 
 try:
-    WeakSet = weakref.WeakSet
-except AttributeError:
+    from weakref import WeakSet
+except ImportError:
     # for py2k
+    from weakref import ref
     class WeakSet(object):
         """Simple weak set implementation.
 
@@ -96,20 +97,143 @@ except AttributeError:
         >>> list(ws)
         []
         """
-        def __init__(self):
-            self.data = weakref.WeakValueDictionary()
-
-        def add(self, key):
-            self.data[id(key)] = key
-
-        def discard(self, key):
-            try:
-                del self.data[id(key)]
-            except KeyError:
-                pass
+        def __init__(self, data=None):
+            self.data = set()
+            def _remove(item, selfref=ref(self)):
+                self = selfref()
+                if self is not None:
+                    self.data.discard(item)
+            self._remove = _remove
+            if data is not None:
+                self.update(data)
 
         def __iter__(self):
-            return self.data.itervalues()
+            for itemref in self.data:
+                item = itemref()
+                if item is not None:
+                    yield item
+
+        def __len__(self):
+            return sum(x() is not None for x in self.data)
+
+        def __contains__(self, item):
+            return ref(item) in self.data
+
+        def __reduce__(self):
+            return (self.__class__, (list(self),),
+                    getattr(self, '__dict__', None))
+
+        def add(self, item):
+            self.data.add(ref(item, self._remove))
+
+        def clear(self):
+            self.data.clear()
+
+        def copy(self):
+            return self.__class__(self)
+
+        def pop(self):
+            while True:
+                try:
+                    itemref = self.data.pop()
+                except KeyError:
+                    raise KeyError('pop from empty WeakSet')
+                item = itemref()
+                if item is not None:
+                    return item
+
+        def remove(self, item):
+            self.data.remove(ref(item))
+
+        def discard(self, item):
+            self.data.discard(ref(item))
+
+        def update(self, other):
+            if isinstance(other, self.__class__):
+                self.data.update(other.data)
+            else:
+                for element in other:
+                    self.add(element)
+        def __ior__(self, other):
+            self.update(other)
+            return self
+
+        # Helper functions for simple delegating methods.
+        def _apply(self, other, method):
+            if not isinstance(other, self.__class__):
+                other = self.__class__(other)
+            newdata = method(other.data)
+            newset = self.__class__()
+            newset.data = newdata
+            return newset
+
+        def difference(self, other):
+            return self._apply(other, self.data.difference)
+        __sub__ = difference
+
+        def difference_update(self, other):
+            if self is other:
+                self.data.clear()
+            else:
+                self.data.difference_update(ref(item) for item in other)
+        def __isub__(self, other):
+            if self is other:
+                self.data.clear()
+            else:
+                self.data.difference_update(ref(item) for item in other)
+            return self
+
+        def intersection(self, other):
+            return self._apply(other, self.data.intersection)
+        __and__ = intersection
+
+        def intersection_update(self, other):
+            self.data.intersection_update(ref(item) for item in other)
+        def __iand__(self, other):
+            self.data.intersection_update(ref(item) for item in other)
+            return self
+
+        def issubset(self, other):
+            return self.data.issubset(ref(item) for item in other)
+        __lt__ = issubset
+
+        def __le__(self, other):
+            return self.data <= set(ref(item) for item in other)
+
+        def issuperset(self, other):
+            return self.data.issuperset(ref(item) for item in other)
+        __gt__ = issuperset
+
+        def __ge__(self, other):
+            return self.data >= set(ref(item) for item in other)
+
+        def __eq__(self, other):
+            if not isinstance(other, self.__class__):
+                return NotImplemented
+            return self.data == set(ref(item) for item in other)
+
+        def symmetric_difference(self, other):
+            return self._apply(other, self.data.symmetric_difference)
+        __xor__ = symmetric_difference
+
+        def symmetric_difference_update(self, other):
+            if self is other:
+                self.data.clear()
+            else:
+                self.data.symmetric_difference_update(ref(item) for item in other)
+        def __ixor__(self, other):
+            if self is other:
+                self.data.clear()
+            else:
+                self.data.symmetric_difference_update(ref(item) for item in other)
+            return self
+
+        def union(self, other):
+            return self._apply(other, self.data.union)
+        __or__ = union
+
+        def isdisjoint(self, other):
+            return len(self.intersection(other)) == 0
 
 class Edge:
     """A directed edge which keeps track of its faces."""
@@ -473,10 +597,10 @@ class Mesh:
         for adj_faces in face.adjacent_faces:
             for adj_face in adj_faces:
                 for adj_adj_faces in adj_face.adjacent_faces:
-                    #adj_adj_faces.discard(face)
-                    # faster:
-                    if id(face) in adj_adj_faces.data:
-                        del adj_adj_faces.data[id(face)]
+                    adj_adj_faces.discard(face)
+                    # faster (but breaks py3k!!):
+                    #if id(face) in adj_adj_faces.data:
+                    #    del adj_adj_faces.data[id(face)]
 
 if __name__=='__main__':
     import doctest
