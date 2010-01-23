0728e330fe6dc950a453722f010a9404b0f1a249
unknown <root@.(none)>
2010-01-23 13:43:55 +0000

    Update install.bat script for Python 2.6.

--- install.bat
+++ install.bat
@@ -4,17 +4,17 @@
 
 rem find python
 set PYTHONPATH=
-for /f "tokens=3* delims=	 " %%A in ('reg.exe query HKLM\SOFTWARE\Python\PythonCore\2.5\InstallPath /ve') do set PYTHONPATH="%%B"
+for /f "tokens=3* delims=	 " %%A in ('reg.exe query HKLM\SOFTWARE\Python\PythonCore\2.6\InstallPath /ve') do set PYTHONPATH="%%B"
 rem not found, Vista 64? TODO fix this for vista
 if not defined PYTHONPATH (
 rem for /f "tokens=3* delims=	 " %%A in ('reg.exe query HKLM\SOFTWARE\Wow6432Node\Python\PythonCore\2.5\InstallPath /ve') do set PYTHONPATH="%%B"
-set PYTHONPATH="C:\Python25"
+set PYTHONPATH="C:\Python26"
 )
 
 rem not found... complain and quit
 if not defined PYTHONPATH (
 echo.
-echo Python 2.5 not found!
+echo Python 2.6 not found!
 echo.
 goto end
 )
