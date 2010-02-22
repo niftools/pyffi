38941502fcb62eef920be76120311eef431fe8dc
Amorilia <amorilia@users.sourceforge.net>
2010-02-22 17:41:09 +0000

    New get_target_color and set_target_color for NiMaterialColorController.

--- pyffi/formats/nif/__init__.py
+++ pyffi/formats/nif/__init__.py
@@ -4604,6 +4604,16 @@ class NifFormat(FileFormat):
                 #key.backward.z *= scale
                 # what to do with TBC?
 
+    class NiMaterialColorController:
+        def get_target_color(self):
+            """Get target color (works for all nif versions)."""
+            return ((self.flags >> 4) & 7) | self.target_color
+
+        def set_target_color(self, target_color):
+            """Set target color (works for all nif versions)."""
+            self.flags |= (target_color & 7) << 4
+            self.target_color = target_color
+
     class NiMorphData:
         def apply_scale(self, scale):
             """Apply scale factor on data."""
