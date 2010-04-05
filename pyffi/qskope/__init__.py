e1489771b5c929d9e2ae16c01826d013e41cbf58
Amorilia <amorilia@users.sourceforge.net>
2010-04-05 12:25:05 +0100

    Added PskFormat to qskope.

--- pyffi/qskope/__init__.py
+++ pyffi/qskope/__init__.py
@@ -54,6 +54,7 @@ from pyffi.formats.egt import EgtFormat
 from pyffi.formats.esp import EspFormat
 from pyffi.formats.tri import TriFormat
 from pyffi.formats.bsa import BsaFormat
+from pyffi.formats.psk import PskFormat
 
 from pyffi.object_models import FileFormat
 
@@ -187,7 +188,7 @@ class QSkope(QtGui.QMainWindow):
             # try reading as a nif file
             for Format in (NifFormat, CgfFormat, KfmFormat, DdsFormat,
                            TgaFormat, EgmFormat, EspFormat, TriFormat,
-                           EgtFormat, BsaFormat):
+                           EgtFormat, BsaFormat, PskFormat):
                 self.data = Format.Data()
                 try:
                     self.data.read(stream)
