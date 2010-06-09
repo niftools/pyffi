b9d5cc781c04144be5464f16d4c94171fc11e186
Pacific Morrowind <pacificmorrowind@gmail.com>
2010-06-09 14:37:28 -0700

    new spell; check_materialemissivevalue checks (and warns) about any emissive values over 0.5 - if they are over  that it is in most cases an error (there are some really really shiny  objects for which it is fine (and desired) to have emissive values over  0.5 but they are rare)

--- scripts/nif/niftoaster.py
+++ scripts/nif/niftoaster.py
@@ -122,7 +122,8 @@ class NifToaster(pyffi.spells.nif.NifToaster):
         pyffi.spells.nif.modify.SpellCollisionToMopp,
         pyffi.spells.nif.optimize.SpellReduceGeometry,
         pyffi.spells.nif.optimize.SpellOptimizeCollisionGeometry,
-        pyffi.spells.nif.optimize.SpellOptimizeAnimation
+        pyffi.spells.nif.optimize.SpellOptimizeAnimation,
+        pyffi.spells.nif.check.SpellCheckMaterialEmissiveValue
         ]
     ALIASDICT = {
         "texdump": "dump_tex",
