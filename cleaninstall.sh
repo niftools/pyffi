fa594922207d751c608f10fcc638788ffe7d0354
Amorilia <amorilia@users.sourceforge.net>
2010-06-11 22:03:43 +0100

    Update for python3 install.

--- cleaninstall.sh
+++ cleaninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clean -f -d -x
-python setup.py -q build
-su -c 'rm -rf /usr/lib/python2.6/site-packages/PyFFI*; rm -rf /usr/lib64/python2.6/site-packages/PyFFI*; rm -rf /usr/lib/python2.6/site-packages/pyffi*; rm -rf /usr/lib64/python2.6/site-packages/pyffi*; python setup.py -q install; chmod +x /usr/lib/python2.6/site-packages/pyffi/utils/mopper.exe'
+python3 setup.py -q build
+su -c 'rm -rf /usr/lib/python3.1/site-packages/PyFFI*; rm -rf /usr/lib64/python3.1/site-packages/PyFFI*; rm -rf /usr/lib/python3.1/site-packages/pyffi*; rm -rf /usr/lib64/python3.1/site-packages/pyffi*; python3 setup.py -q install; chmod +x /usr/lib/python3.1/site-packages/pyffi/utils/mopper.exe'
 rm -rf build
 
