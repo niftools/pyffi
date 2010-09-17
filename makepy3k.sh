0228260f83215649f2bc02eb20c1faee14169d30
Amorilia <amorilia@users.sourceforge.net>
2010-09-17 15:58:31 +0200

    Remove some scripts which are no longer of any use.

--- makepy3k.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git clean -x -d -f
-../Python-3.1.2/python ../Python-3.1.2/Tools/scripts/2to3 -w -n pyffi examples scripts rundoctest.py setup.py
-find tests/ -name "*.txt" | xargs ../Python-3.1.2/python ../Python-3.1.2/Tools/scripts/2to3 -w -n -d
-#../Python-3.1.2/python rundoctest.py
-
