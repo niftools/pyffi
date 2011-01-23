387a755c6ff14d309e1f7a187af9979c13464dbf
Amorilia <amorilia@users.sourceforge.net>
2011-01-23 16:13:49 +0000

    Remaining copyright message update.

--- pyffi/formats/psk/__init__.py
+++ pyffi/formats/psk/__init__.py
@@ -44,7 +44,7 @@ Create an PSK file from scratch and write to file
 
 # ***** BEGIN LICENSE BLOCK *****
 #
-# Copyright (c) 2007-2009, Python File Format Interface
+# Copyright (c) 2007-2011, Python File Format Interface
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
