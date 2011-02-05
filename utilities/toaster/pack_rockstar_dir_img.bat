01dac9810abd6697a3fcdc20d350b9a264cf21c5
Amorilia <amorilia@users.sourceforge.net>
2011-02-05 08:56:12 +0000

    Documented scripts also in .bat file.

--- utilities/toaster/pack_rockstar_dir_img.bat
+++ utilities/toaster/pack_rockstar_dir_img.bat
@@ -1,3 +1,13 @@
 @echo off
+rem
+rem Packs for instance
+rem
+rem   out/World/*.*
+rem
+rem (or any other non-empty folder in out/) to
+rem
+rem   archive_out/World.dir
+rem   archive_out/World.img
+rem
 "PYTHONPATH\python.exe" "PYTHONPATH\Scripts\pack_rockstar_dir_img.py" out archive_out
 pause
