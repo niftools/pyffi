b0eaed4ae91a4f7e3a2a1ad502245a67ec031d0e
Amorilia <amorilia@users.sourceforge.net>
2012-09-05 21:41:09 +0100

    Use pip for installing cleanly.

--- cleaninstall.sh
+++ cleaninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 git clean -f -d -x
 python3 setup.py -q build
-su -c 'rm -rf /usr/lib/python3.2/site-packages/PyFFI*; rm -rf /usr/lib64/python3.2/site-packages/PyFFI*; rm -rf /usr/lib/python3.2/site-packages/pyffi*; rm -rf /usr/lib64/python3.2/site-packages/pyffi*; python3 setup.py -q install'
+sudo 'rm -rf /usr/lib/python3.2/site-packages/PyFFI*; rm -rf /usr/lib64/python3.2/site-packages/PyFFI*; rm -rf /usr/lib/python3.2/site-packages/pyffi*; rm -rf /usr/lib64/python3.2/site-packages/pyffi*'
+rm dist/*
+python3 setup.py -q sdist --format=zip
+sudo pip-python3 install dist/PyFFI-*.zip
 rm -rf build
 
