28e5a2ba5e2bdf17beedfe8aaf923427822571e1
Amorilia <amorilia@users.sourceforge.net>
2012-10-18 21:58:43 +0100

    Update for rundoctest script (assuming buildenv).

--- rundoctest.bat
+++ rundoctest.bat
@@ -1,33 +1,4 @@
-@rem quick pyffi doctest batch file
-
 @echo off
-
-rem find python
-set PYTHONPATH=
-for /f "tokens=3* delims=	 " %%A in ('reg.exe query "HKLM\SOFTWARE\Python\PythonCore\2.5\InstallPath\" \ve') do set PYTHONPATH="%%B"
-
-rem not found... complain and quit
-if not defined PYTHONPATH (
-echo.
-echo Python 2.5 not found!
-echo.
-goto end
-)
-
-rem python in registry but executable not found... complain and exit
-if not exist %PYTHONPATH%\python.exe (
-echo.
-echo python.exe not found!
-echo.
-goto end
-)
-
-rem run the installation from the setup script
-%PYTHONPATH%\python.exe rundoctest.py
-
-:end
-
-rem remove local variables
-set PYTHONPATH=
-
+rem quick pyffi doctest batch file
+python.exe rundoctest.py
 pause
