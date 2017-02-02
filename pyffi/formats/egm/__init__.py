b70d4ae1a713fdd890f9a50a05eb3dc57a81a452
neomonkeus <neomonkeus@gmail.com>
2017-02-02 23:51:59 +0000

    Egm tests passing with platform independent paths.

--- pyffi/formats/egm/__init__.py
+++ pyffi/formats/egm/__init__.py
@@ -19,7 +19,11 @@ Read a EGM file
 ^^^^^^^^^^^^^^^
 
 >>> # check and read egm file
->>> stream = open('tests/egm/mmouthxivilai.egm', 'rb')
+>>> from os.path import dirname, abspath
+>>> root = dirname(dirname(dirname(dirname(abspath(__file__)))))
+>>> format_root = os.path.join(root, 'tests', 'egm')
+>>> file = os.path.join(format_root, 'mmouthxivilai.egm')
+>>> stream = open(file, 'rb')
 >>> data = EgmFormat.Data()
 >>> data.inspect_quick(stream)
 >>> data.version
@@ -40,9 +44,18 @@ Read a EGM file
 Parse all EGM files in a directory tree
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
->>> for stream, data in EgmFormat.walkData('tests/egm'):
-...     print(stream.name)
-tests/egm/mmouthxivilai.egm
+>>> for stream, data in EgmFormat.walkData(format_root):
+...     try:
+...         # the replace call makes the doctest also pass on windows
+...         os_path = stream.name
+...         split = (os_path.split(os.sep))[-3:]
+...         rejoin = os.path.join(*split).replace(os.sep, "/")
+...         print("reading %s" % rejoin)
+...     except Exception:
+...         print(
+...             "Warning: read failed due corrupt file,"
+...             " corrupt format description, or bug.") # doctest: +REPORT_NDIFF
+reading tests/egm/mmouthxivilai.egm
 
 Create an EGM file from scratch and write to file
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
