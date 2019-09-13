7b2609575bc4d40592dcee2cbdb03d94db071c15
HENDRIX-ZT2 <arnfinn@spholderer.de>
2019-09-13 16:27:36 +0200

    Fix: Import bhkMalleableConstraint
    Not tested because I didn't find one in the wild, but it has the exact same pattern found in bhkLimitedHingeConstraint, and no sub_constraint attribute is noted in the nif.xml, but the corrected ones are.

--- pyffi/formats/nif/__init__.py
+++ pyffi/formats/nif/__init__.py
@@ -2406,24 +2406,24 @@ class NifFormat(FileFormat):
         def apply_scale(self, scale):
             """Scale data."""
             # apply scale on transform
-            self.sub_constraint.ragdoll.pivot_a.x *= scale
-            self.sub_constraint.ragdoll.pivot_a.y *= scale
-            self.sub_constraint.ragdoll.pivot_a.z *= scale
-            self.sub_constraint.ragdoll.pivot_b.x *= scale
-            self.sub_constraint.ragdoll.pivot_b.y *= scale
-            self.sub_constraint.ragdoll.pivot_b.z *= scale
-            self.sub_constraint.limited_hinge.pivot_a.x *= scale
-            self.sub_constraint.limited_hinge.pivot_a.y *= scale
-            self.sub_constraint.limited_hinge.pivot_a.z *= scale
-            self.sub_constraint.limited_hinge.pivot_b.x *= scale
-            self.sub_constraint.limited_hinge.pivot_b.y *= scale
-            self.sub_constraint.limited_hinge.pivot_b.z *= scale
+            self.ragdoll.pivot_a.x *= scale
+            self.ragdoll.pivot_a.y *= scale
+            self.ragdoll.pivot_a.z *= scale
+            self.ragdoll.pivot_b.x *= scale
+            self.ragdoll.pivot_b.y *= scale
+            self.ragdoll.pivot_b.z *= scale
+            self.limited_hinge.pivot_a.x *= scale
+            self.limited_hinge.pivot_a.y *= scale
+            self.limited_hinge.pivot_a.z *= scale
+            self.limited_hinge.pivot_b.x *= scale
+            self.limited_hinge.pivot_b.y *= scale
+            self.limited_hinge.pivot_b.z *= scale
 
         def update_a_b(self, parent):
             """Update the B data from the A data."""
             transform = self.get_transform_a_b(parent)
-            self.sub_constraint.limited_hinge.update_a_b(transform)
-            self.sub_constraint.ragdoll.update_a_b(transform)
+            self.limited_hinge.update_a_b(transform)
+            self.ragdoll.update_a_b(transform)
 
     class bhkMoppBvTreeShape:
         def get_mass_center_inertia(self, density=1, solid=True):
