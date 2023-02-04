c81a5e96a42a2c8d147056cce1d5f0b75f8398bc
Tagan Hoyle <tagnum.elite@gmail.com>
2023-02-04 22:45:04 +0200

    Add more testing on blocks in test_games

--- tests/formats/nif/test_games.py
+++ tests/formats/nif/test_games.py
@@ -217,7 +217,74 @@ def test_import_morrowind():
     data = read_file("morrowind_mossyrock.nif")
     assert data.version == NifFormat.version_number("4.0.0.2")
     assert data.header.num_blocks == 11
-    # TODO: All/some blocks
+
+    # 0: NiNode
+    block = data.blocks[0]
+    assert isinstance(block, NifFormat.NiNode)
+    assert block.name == b"chasmwallwg_mw"
+    assert block.flags == 12
+    assert block.translation == NifFormat.Vector3.zero()
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+    assert block.velocity == NifFormat.Vector3.zero()
+    assert block.num_properties == 0
+    assert not block.has_bounding_volume
+    assert block.num_children == 2
+    assert block.num_effects == 0
+
+    # 1: NiTriShape
+    block = data.blocks[1]
+    assert isinstance(block, NifFormat.NiTriShape)
+    assert block.name == b"Tri Cube 0"
+    assert block.flags == 4
+    assert block.translation == NifFormat.Vector3.zero()
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+    assert block.velocity == NifFormat.Vector3.zero()
+    assert block.num_properties == 3
+    assert not block.has_bounding_volume
+
+    # 6: NiTriShapeData
+    block = data.blocks[6]
+    assert isinstance(block, NifFormat.NiTriShapeData)
+    assert block.num_vertices == 755
+    assert block.has_vertices
+    assert block.has_normals
+    assert block.bounding_sphere.center == NifFormat.Vector3.create(3.295708, 10.778694, 531.150818)
+    assert block.bounding_sphere.radius == pytest.approx(1011.122681)
+    assert block.has_vertex_colors
+    assert block.data_flags.num_uv_sets == 1
+    assert block.has_uv
+    assert block.num_triangles == 1340
+    assert block.num_triangle_points == 4020
+    assert block.num_match_groups == 0
+
+    # 7: NiTriShape
+    block = data.blocks[7]
+    assert isinstance(block, NifFormat.NiTriShape)
+    assert block.name == b"Tri Cube 1"
+    assert block.flags == 4
+    assert block.translation == NifFormat.Vector3.zero()
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+    assert block.velocity == NifFormat.Vector3.zero()
+    assert block.num_properties == 3
+    assert not block.has_bounding_volume
+
+    # 10: NiTriShapeData
+    block = data.blocks[10]
+    assert isinstance(block, NifFormat.NiTriShapeData)
+    assert block.num_vertices == 365
+    assert block.has_vertices
+    assert block.has_normals
+    assert block.bounding_sphere.center == NifFormat.Vector3.create(-9.280243, -7.708931, 1167.847168)
+    assert block.bounding_sphere.radius == pytest.approx(934.304688)
+    assert block.has_vertex_colors
+    assert block.data_flags.num_uv_sets == 1
+    assert block.has_uv
+    assert block.num_triangles == 678
+    assert block.num_triangle_points == 2034
+    assert block.num_match_groups == 0
 
 
 def test_import_oblivion():
@@ -227,7 +294,53 @@ def test_import_oblivion():
     assert data.bs_version == 11
     assert data.header.num_blocks == 39
     assert data.header.num_block_types == 14
-    # TODO: All/some blocks
+
+    # 0: NiNode
+    block = data.blocks[0]
+    assert isinstance(block, NifFormat.NiNode)
+    assert block.name == b"Scene Root"
+    assert block.num_extra_data_list == 2
+    assert block.flags == 8
+    assert block.translation == NifFormat.Vector3.zero()
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+    assert block.num_properties == 0
+    assert block.num_children == 6
+    assert block.num_effects == 0
+
+    # 1: BSXFlags
+    block = data.blocks[1]
+    assert isinstance(block, NifFormat.BSXFlags)
+    assert block.name == b"BSX"
+    assert block.integer_data == 2
+
+    # 9: NiTriShape
+    block = data.blocks[9]
+    assert isinstance(block, NifFormat.NiTriShape)
+    assert block.name == b"Chair:1"
+    assert block.num_extra_data_list == 1
+    assert block.flags == 0
+    assert block.translation == NifFormat.Vector3.zero()
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+    assert block.num_properties == 2
+
+    # 14: NiTriShapeData
+    block = data.blocks[14]
+    assert isinstance(block, NifFormat.NiTriShapeData)
+    assert block.group_id == 0
+    assert block.num_vertices == 2203
+    assert block.keep_flags == 0
+    assert block.compress_flags == 0
+    assert block.has_vertices
+    assert block.has_normals
+    assert block.bounding_sphere.center == NifFormat.Vector3.create(0.000055, 4.207962, 59.622810)
+    assert block.bounding_sphere.radius == pytest.approx(65.464142)
+    assert not block.has_vertex_colors
+    assert block.num_triangles == 3778
+    assert block.num_triangle_points == 11334
+    assert block.has_triangles
+    assert block.num_match_groups == 0
 
 
 def test_import_skyrim():
@@ -238,7 +351,52 @@ def test_import_skyrim():
     assert data.header.num_blocks == 12
     assert data.header.num_block_types == 12
     assert data.header.num_strings == 4
-    # TODO: All/some blocks
+
+    # 0: BSFadeNode
+    block = data.blocks[0]
+    assert isinstance(block, NifFormat.BSFadeNode)
+    assert block.name == b"Scene Root"
+    assert block.num_extra_data_list == 2
+    assert block.flags == 524302
+    assert block.translation == NifFormat.Vector3.zero()
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+    assert block.num_children == 1
+    assert block.num_effects == 0
+
+    # 1: BSXFlags
+    block = data.blocks[1]
+    assert isinstance(block, NifFormat.BSXFlags)
+    assert block.name == b"BSX"
+    assert block.integer_data == 194
+
+    # 7: NiTriShape
+    block = data.blocks[7]
+    assert isinstance(block, NifFormat.NiTriShape)
+    assert block.name == b"cookie005"
+    assert block.num_extra_data_list == 0
+    assert block.flags == 524302
+    assert block.translation == NifFormat.Vector3.create(0.043507, 19.610661, 0.627650)
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+
+    # 10: NiTriShapeData
+    block = data.blocks[10]
+    assert isinstance(block, NifFormat.NiTriShapeData)
+    assert block.group_id == 0
+    assert block.num_vertices == 344
+    assert block.keep_flags == 0
+    assert block.compress_flags == 0
+    assert block.has_vertices
+    assert block.material_crc == 0
+    assert block.has_normals
+    assert block.bounding_sphere.center == NifFormat.Vector3.create(0.001349, -19.619707, 0.754842)
+    assert block.bounding_sphere.radius == pytest.approx(3.932583)
+    assert not block.has_vertex_colors
+    assert block.num_triangles == 576
+    assert block.num_triangle_points == 1728
+    assert block.has_triangles
+    assert block.num_match_groups == 0
 
 
 def test_import_skyrimse():
@@ -249,4 +407,36 @@ def test_import_skyrimse():
     assert data.header.num_blocks == 11
     assert data.header.num_block_types == 11
     assert data.header.num_strings == 4
-    # TODO: All/some blocks
+
+    # 0: BSFadeNode
+    block = data.blocks[0]
+    assert isinstance(block, NifFormat.BSFadeNode)
+    assert block.name == b"Scene Root"
+    assert block.num_extra_data_list == 2
+    assert block.flags == 524302
+    assert block.translation == NifFormat.Vector3.zero()
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+    assert block.num_children == 1
+    assert block.num_effects == 0
+
+    # 1: BSXFlags
+    block = data.blocks[1]
+    assert isinstance(block, NifFormat.BSXFlags)
+    assert block.name == b"BSX"
+    assert block.integer_data == 194
+
+    # 7: BSTriShape
+    block = data.blocks[7]
+    assert isinstance(block, NifFormat.BSTriShape)
+    assert block.name == b"cookie005"
+    assert block.num_extra_data_list == 0
+    assert block.flags == 524302
+    assert block.translation == NifFormat.Vector3.create(0.043507, 19.610661, 0.627650)
+    assert block.rotation == NifFormat.Matrix33.identity()
+    assert block.scale == 1.0
+    assert block.num_triangles == 576
+    assert block.num_vertices == 344
+    assert block.data_size == 13088
+    assert block.particle_data_size == 0
+
